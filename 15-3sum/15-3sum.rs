88bd3144bb7c7d7fa542818c49361e8f5f90138e
Tomi Jaga <jagatomi12@gmail.com>
2022-04-20 17:17:04 -0400

    Time: 1402 ms (5.86%), Space: 3.5 MB (48.95%) - LeetHub

---
+++ 15-3sum/15-3sum.rs
@@ -0,0 +1,41 @@
+use std::collections::HashMap;
+
+pub fn exists(nums: &Vec<Vec<i32>>, elem: &Vec<i32> ) -> bool{
+    for n in nums{
+        if (n[0]== elem[0] && n[1]== elem[1] && n[2]== elem[2]){
+            return true;
+        }
+    }
+    
+    return false;
+}
+
+impl Solution {
+    pub fn three_sum(nums: Vec<i32>) -> Vec<Vec<i32>> {
+        
+        let mut targets = HashMap::new();
+        let mut sum = vec![];
+        
+        for (i, n) in nums.iter().enumerate(){
+            targets.insert(n, i);
+        }
+        
+        for i in 0..nums.len(){
+            for j in (i + 1)..nums.len(){
+                let target = 0 -( nums[i] + nums[j]);
+                
+                if let Some (index) = targets.get(&target){
+                    if (i != j && j!= *index && *index!=i){
+                        let mut elem = vec![nums[*index], nums[i], nums[j]];
+                        elem.sort();
+                        if (!exists(&sum, &elem)){
+                            sum.push(elem);
+                        }
+                    }
+                }
+            }
+        } 
+        
+        sum
+    }
+}
