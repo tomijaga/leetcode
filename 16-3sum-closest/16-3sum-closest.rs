53c397a6ca08218d09d847c2f09e2f1afe973e31
Tomi Jaga <jagatomi12@gmail.com>
2022-04-20 20:42:07 -0400

    Time: 8 ms (26.67%), Space: 2.1 MB (42.22%) - LeetHub

---
+++ 16-3sum-closest/16-3sum-closest.rs
@@ -0,0 +1,51 @@
+use std::{i32, cmp};
+pub fn abs(val:i32)->i32{
+    let _abs = val.abs();
+    if _abs == i32::MIN{
+        return _abs +1;
+    }
+    _abs
+}
+
+pub fn diff(b: i32, a: i32)->i32{
+    abs(b - a)
+}
+
+pub fn is_closer(prev: i32, curr: i32, target: i32)->bool{
+    
+   diff(target, curr) < diff(target, prev)
+}
+
+
+impl Solution {
+    pub fn three_sum_closest(nums: Vec<i32>, target: i32) -> i32 {
+        let mut nums = nums;
+        nums.sort();
+        
+        let mut closest = nums[0]+ nums[1]+nums[2];
+        
+        for i in 0..nums.len() -1{
+            let mut j = i + 1;
+            let mut k = nums.len() -1;
+            
+            while(j < k){
+                let sum = nums[i]+ nums[j] + nums[k];
+                let diff = (target - sum);
+                // println!("{}", (target-closest ).abs());
+                if (sum > target){
+                    k-=1;
+                }else if (sum < target){
+                    j+=1;
+                }else{
+                    return sum;
+                }
+                
+                if(is_closer(closest, sum, target)){
+                    closest = sum;
+                }
+            }
+        }
+        
+        closest
+    }
+}
