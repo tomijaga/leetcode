23ca0fb8b6777269b99a27afa1511e3abe632ade
Tomi Jaga <jagatomi12@gmail.com>
2022-04-22 19:12:24 -0400

    Time: 10 ms (23.68%), Space: 2.4 MB (44.74%) - LeetHub

---
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.rs
@@ -0,0 +1,36 @@
+pub fn reset_zeroes(matrix: &mut Vec<Vec<i32>>, x: usize, y: usize){
+    let m = matrix.len();
+    let n = matrix[0].len();
+    
+    for i in 0..m{
+        matrix[i][y] = 0;
+    }
+    
+    for j in 0..n{
+        matrix[x][j] = 0;
+    }
+}
+
+impl Solution {
+    pub fn set_zeroes(matrix: &mut Vec<Vec<i32>>) {
+        let mut zeroes = vec![];
+        
+        let m = matrix.len();
+        let n = matrix[0].len();
+        
+        for i in 0..m{
+            for j in 0..n{
+                if matrix[i][j] == 0{
+                    zeroes.push((i, j));
+                }
+            }
+        }
+        
+        println!("{:?}", zeroes);
+        
+        for (x, y) in zeroes.iter(){
+            reset_zeroes(matrix, *x, *y);
+        }
+        
+    }
+}
