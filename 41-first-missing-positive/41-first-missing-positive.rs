0aa1bab9682e7b4f0e87e0add23fcbe1c132661d
Tomi Jaga <jagatomi12@gmail.com>
2022-04-25 13:34:10 -0400

    Time: 24 ms (33.91%), Space: 7.3 MB (70.43%) - LeetHub

---
+++ 41-first-missing-positive/41-first-missing-positive.rs
@@ -0,0 +1,16 @@
+impl Solution {
+    pub fn first_missing_positive(nums: Vec<i32>) -> i32 {
+        let mut nums = nums;
+        
+        nums.sort();
+        let mut min = 1;
+        for n in nums{
+            if n == min{
+                min+=1;
+            }else if n>min{
+                return min;
+            }
+        }
+        return min;
+    }
+}
