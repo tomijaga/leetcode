9900f909f472df9a0aea8820619eb868b180645d
Tomi Jaga <jagatomi12@gmail.com>
2022-04-25 10:40:04 -0400

    Time: 0 ms (100.00%), Space: 2.2 MB (28.57%) - LeetHub

--- 1550-three-consecutive-odds/1550-three-consecutive-odds.rs
+++ 1550-three-consecutive-odds/1550-three-consecutive-odds.rs
@@ -1,15 +1,16 @@
+pub fn isOdd(n: i32)-> bool{
+    n %2 == 1
+}
+
 impl Solution {
     pub fn three_consecutive_odds(arr: Vec<i32>) -> bool {
         let mut odds = 0;
+        if arr.len() < 3{
+            return false;
+        }
         
-        for n in arr{
-            if n %2 == 1{
-                odds+=1;
-            }else{
-                odds = 0;
-            }
-            
-            if odds ==3{
+        for i in 0..arr.len() - 2{
+            if isOdd(arr[i]) && isOdd(arr[i+1]) && isOdd(arr[i+2]){
                 return true;
             }
         }
