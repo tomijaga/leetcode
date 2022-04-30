ba31cd55ab2b02d7edfdddb74dc86459f0d69deb
Tomi Jaga <jagatomi12@gmail.com>
2022-04-30 19:58:53 -0400

    Time: 36 ms (100.00%), Space: 4.1 MB (100.00%) - LeetHub

--- 2256-minimum-average-difference/2256-minimum-average-difference.rs
+++ 2256-minimum-average-difference/2256-minimum-average-difference.rs
@@ -2,13 +2,12 @@
 impl Solution {
     pub fn minimum_average_difference(nums: Vec<i32>) -> i32 {
         
-        let mut sum_arr = vec![];
         let mut sum: i64 = 0;
         
-        for n in nums{
-            sum += n as i64;
-            sum_arr.push(sum);
-        }
+        let sum_arr = nums.into_iter().map(|a|{
+            sum += a as i64; 
+            sum
+        }).collect::<Vec<i64>>();
         
         let mut min = i64::MAX;
         let mut min_index = 0;
