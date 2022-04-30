e9d6b74d9f0c8351adff02774516c04a73e6c054
Tomi Jaga <jagatomi12@gmail.com>
2022-04-29 20:50:24 -0400

    Time: 812 ms (5.41%), Space: 2.2 MB (43.24%) - LeetHub

---
+++ 459-repeated-substring-pattern/459-repeated-substring-pattern.rs
@@ -0,0 +1,32 @@
+impl Solution {
+    pub fn repeated_substring_pattern(s: String) -> bool {
+        let chars = s.chars().collect::<Vec<char>>();
+        
+        for n in 1..(chars.len()/2 + 1){
+            let mut start = n;
+            let mut prev = chars[0..n].to_vec();
+            let mut curr = vec![];
+            
+            let mut repeated = true;
+            
+            while(repeated && start < chars.len()){
+                
+                if (start + n > chars.len()){repeated = false; break;}
+                curr = chars[start..(start + n)].to_vec();
+                start += n;
+                
+                // println!("{:?}, {:?}", prev, curr);
+                
+                repeated = prev == curr && repeated;
+                prev = curr;
+            }
+            
+            if (repeated == true){
+                return true;
+            }
+            
+        }
+        
+        false
+    }
+}
