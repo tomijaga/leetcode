221678dd12b8fc7b1ee4c58d7f5a967a07468cbf
Tomi Jaga <jagatomi12@gmail.com>
2022-04-29 20:01:43 -0400

    Time: 0 ms (100.00%), Space: 2.8 MB (57.58%) - LeetHub

--- 110-balanced-binary-tree/110-balanced-binary-tree.rs
+++ 110-balanced-binary-tree/110-balanced-binary-tree.rs
@@ -22,21 +22,25 @@ use std::cell::RefCell;
 use std::cmp::max;
 
 impl TreeNode{
-    pub fn get_balanced_height(&self)-> i32{
+    pub fn dfs_height(&self)-> i32{
         let mut lh = 0;
         let mut rh = 0;
         
         if let Some(ref left) = self.left{
-            lh = left.borrow().get_balanced_height();
+            lh = left.borrow().dfs_height();
+            if lh == -1 {
+                return -1;
+            }
         }
         
         if let Some(ref right) = self.right{
-            rh = right.borrow().get_balanced_height();
+            rh = right.borrow().dfs_height();
+            if rh == -1 {
+                return -1;
+            }
         }
         
-        if lh == -1 || rh == -1 {
-            -1
-        }else if (lh - rh).abs() > 1 {
+        if (lh - rh).abs() > 1 {
             -1
         }else{
             max(rh, lh)  + 1
@@ -47,8 +51,7 @@ impl TreeNode{
 impl Solution {
     pub fn is_balanced(root: Option<Rc<RefCell<TreeNode>>>) -> bool {
         if let Some(ref node) = root {
-            let h = node.borrow().get_balanced_height();
-            // println!("h: {}", h);÷
+            let h = node.borrow().dfs_height();
             h != -1
         }else{
             true
