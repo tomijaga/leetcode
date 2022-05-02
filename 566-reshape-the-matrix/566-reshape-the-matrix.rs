7d24be44954ce8915223613e382b296305512261
Tomi Jaga <jagatomi12@gmail.com>
2022-05-02 12:00:24 -0400

    Time: 6 ms (44.12%), Space: 2.1 MB (97.06%) - LeetHub

--- 566-reshape-the-matrix/566-reshape-the-matrix.rs
+++ 566-reshape-the-matrix/566-reshape-the-matrix.rs
@@ -1,16 +1,13 @@
 pub type Point = (usize, usize);
 
-pub fn get_next(limits: Point, curr: Point) -> Point{
+pub fn get_next(limits: Point, i: &mut usize, j: &mut usize){
     let (m, n) = limits;
-    let (mut i, mut j) = curr;
-    if (j+1>=n){
-        j = 0;
-        i+=1;
+    if (*j+1>=n){
+        *j = 0;
+        *i+=1;
     }else{
-        j+=1;
+        *j+=1;
     }
-    
-    (i, j)
 }
     
 impl Solution {
@@ -34,10 +31,7 @@ impl Solution {
                 
                 new_mat[x][y] = val;
                 
-                let point = get_next((r, c), (x, y));
-                // println!("{:?}", point);
-                x = point.0;
-                y = point.1;
+                get_next((r, c), &mut x, &mut y);
             }
         }
         
