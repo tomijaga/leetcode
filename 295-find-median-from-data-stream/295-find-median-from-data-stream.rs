5d438d026c06a999004d3eece49dd7421c5b06cb
Tomi Jaga <jagatomi12@gmail.com>
2022-05-06 03:00:23 -0400

    Time: 149 ms (40.63%), Space: 23.6 MB (34.38%) - LeetHub

--- 295-find-median-from-data-stream/295-find-median-from-data-stream.rs
+++ 295-find-median-from-data-stream/295-find-median-from-data-stream.rs
@@ -1,7 +1,10 @@
-struct MedianFinder {
-    nums: Vec<i32>
-}
+use std::collections::BinaryHeap;
+use std::cmp::Reverse;
 
+struct MedianFinder {
+    big: BinaryHeap<Reverse<i32>>,
+    small: BinaryHeap<i32>
+}
 
 /** 
  * `&self` means the method takes an immutable reference.
@@ -11,25 +14,52 @@ impl MedianFinder {
 
     fn new() -> Self {
         Self{
-            nums: vec![]
+            big: BinaryHeap::new(),
+            small: BinaryHeap::new()
         }
+    }
+    
+    fn len(&self)-> usize{
+        self.small.len() +  self.big.len()
     }
     
     fn add_num(&mut self, num: i32) {
-        self.nums.push(num);
+        self.small.push(num);
+        
+        let n = self.small.pop().unwrap();
+        self.big.push(Reverse(n));
+        
+        let s_len = self.small.len();
+        let mid = self.len()/2;
+        // println!("l:{}, s:{}, m:{}", self.len(), s_len, mid);
+        
+        if s_len <= mid{
+            if let Some(Reverse(n)) = self.big.pop(){
+                self.small.push(n);
+            }
+        }
     }
     
     fn find_median(&mut self) -> f64 {
-        self.nums.sort_unstable();
-        
-        let len = self.nums.len();
+        let len = self.len();
         let mid = len/2;
         
+        let mut elems = vec![];
+        
+        // println!("s: {:?}\nb:{:?}\n", &self.small, &self.big);
+        
         if len % 2 == 0{
-            (self.nums[mid] + self.nums[mid-1]) as f64 / 2_f64
+            elems.push(self.small.pop().unwrap());
+            elems.push(self.small.pop().unwrap());
         }else{
-            self.nums[mid] as f64
+            elems.push(self.small.pop().unwrap());
         }
+        
+        for &n in elems.iter(){
+            self.small.push(n);
+        }
+        
+        elems.iter().sum::<i32>() as f64 / elems.len() as f64
     }
 }
 
