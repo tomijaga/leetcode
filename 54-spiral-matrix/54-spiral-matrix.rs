ac59786afd03227aa12a46f285a3121d51714b2f
Tomi Jaga <jagatomi12@gmail.com>
2022-05-08 02:11:10 -0400

    Time: 2 ms (16.67%), Space: 2.2 MB (45.83%) - LeetHub

---
+++ 54-spiral-matrix/54-spiral-matrix.rs
@@ -0,0 +1,57 @@
+pub type Point<T> = (T, T);
+
+pub fn is_valid(top: usize, bottom: usize, left: usize, right: usize)->bool{
+    top < bottom && left < right
+}
+
+impl Solution {
+    pub fn spiral_order(matrix: Vec<Vec<i32>>) -> Vec<i32> {
+        let mut spiral = vec![];
+        
+        let mut top = 0;
+        let mut right = matrix[0].len();
+        let mut left = 0;
+        let mut bottom = matrix.len();
+        
+        while is_valid(top, bottom, left, right){
+            if is_valid(top, bottom, left, right){
+                for j in left..right{
+                    spiral.push(matrix[top][j]);
+                }
+                top+=1;
+            }
+            
+            println!("{:?}", spiral);
+        
+            if is_valid(top, bottom, left, right){
+            
+                for i in top..bottom{
+                    spiral.push(matrix[i][right -1]);
+                }
+                right-=1;
+            }
+
+            println!("{:?}", spiral);
+
+            if is_valid(top, bottom, left, right){
+
+                for j in (left..right).rev(){
+                    spiral.push(matrix[bottom -1][j]);
+                }
+                bottom-=1;
+            }
+            println!("{:?}", spiral);
+            
+            if is_valid(top, bottom, left, right){
+                for i in (top..bottom).rev(){
+                    spiral.push(matrix[i][left]);
+                }
+                left+=1;
+            }
+
+            println!("{:?}", spiral);
+        }
+        
+        spiral
+    }
+}
