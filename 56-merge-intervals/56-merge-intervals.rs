fe30bbf24e11547c81c24e4ee9b08a4543f7d48b
Tomi Jaga <jagatomi12@gmail.com>
2022-05-24 14:40:25 -0400

    Time: 15 ms (27.96%), Space: 3.4 MB (20.43%) - LeetHub

---
+++ 56-merge-intervals/56-merge-intervals.rs
@@ -0,0 +1,29 @@
+use std::cmp::{max, min};
+
+impl Solution {
+    pub fn merge(intervals: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
+        
+        if intervals.len() <= 1{
+            return intervals;
+        }
+        
+        let mut intervals = intervals;
+        intervals.sort_unstable_by(|a, b|{ a[0].cmp(&b[0])});
+        
+        let mut result = vec![intervals[0].clone()];
+        
+        for i in 1..intervals.len(){
+            let prev = result.last().unwrap();
+            let next = &intervals[i];
+            
+            if prev[1] >= next[0] {
+                let newInterval = vec![prev[0], max(prev[1], next[1])];
+                result.pop();
+                result.push(newInterval);
+            }else{
+                result.push(intervals[i].clone());
+            }
+        }
+        result
+    }
+}
