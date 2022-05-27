eefd5b67c388dfc95d7008083710de235110989c
Tomi Jaga <jagatomi12@gmail.com>
2022-05-26 22:59:01 -0400

    Time: 0 ms (100.00%), Space: 2.1 MB (50.88%) - LeetHub

---
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.rs
@@ -0,0 +1,18 @@
+pub fn calc_steps(step: (i32, i32))-> (i32,i32){
+    let (num, count) = step;
+    
+    if num == 0{
+        step
+    }else if num % 2 == 0{
+        calc_steps((num/2, count +1))
+    }else{
+        calc_steps((num -1 , count +1))
+    }
+}
+
+impl Solution {
+    pub fn number_of_steps(num: i32) -> i32 {
+        let (_, count ) = calc_steps((num, 0));
+        count
+    }
+}
