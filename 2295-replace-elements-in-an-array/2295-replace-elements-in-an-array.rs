ade213cf3b8dde41151de54281542cd437a605f0
Tomi Jaga <jagatomi12@gmail.com>
2022-06-05 00:16:58 -0400

    Time: 135 ms (100.00%), Space: 17.8 MB (100.00%) - LeetHub

---
+++ 2295-replace-elements-in-an-array/2295-replace-elements-in-an-array.rs
@@ -0,0 +1,25 @@
+use std::collections::HashMap;
+
+impl Solution {
+    pub fn array_change(mut nums: Vec<i32>, operations: Vec<Vec<i32>>) -> Vec<i32> {
+        let mut map = HashMap::new();
+        
+        for i in 0..nums.len(){
+            map.insert(nums[i], i);
+        }
+        
+        for v in operations{
+            let old = v[0];
+            let new = v[1];
+            
+            let i = map.remove(&old).unwrap();
+            map.insert(new, i);
+        }
+        
+        for (key, i) in map{
+            nums[i] = key;
+        }
+        
+        nums
+    }
+}
