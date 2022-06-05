5d2b67126e1ea8fef934cac98457f957289fc54c
Tomi Jaga <jagatomi12@gmail.com>
2022-06-04 21:37:10 -0400

    Time: 5 ms (47.37%), Space: 2.6 MB (89.47%) - LeetHub

--- 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.rs
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.rs
@@ -2,22 +2,22 @@ impl Solution {
     pub fn search_matrix(matrix: Vec<Vec<i32>>, target: i32) -> bool {
         let (m, n) = (matrix.len(), matrix[0].len());
 
-        for i in 0..m{
-            let mut l = 0;
-            let mut r = n;
-            
-            while l < r{
-                let mid = l + (r - l)/2;
+        let mut row = m - 1;
+        let mut col = 0;
+        
+            while col < n && row >=0  {
                 
-                if matrix[i][mid] < target{
-                    l= mid + 1;
-                }else if matrix[i][mid] > target{
-                    r = mid;
+                if matrix[row][col] < target{
+                    col+= 1;
+                }else if matrix[row][col] > target{
+                    if row == 0{
+                        return false;
+                    }
+                    row-=1;
                 }else{
                     return true;
                 }
             }
-        }
         
         false
         
