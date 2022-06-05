08b5f6c1bc79217f8ba440bb230436dd469f95fe
Tomi Jaga <jagatomi12@gmail.com>
2022-06-04 22:10:20 -0400

    Time: 3 ms (60.00%), Space: 2.7 MB (100.00%) - LeetHub

--- 275-h-index-ii/275-h-index-ii.rs
+++ 275-h-index-ii/275-h-index-ii.rs
@@ -1,15 +1,29 @@
+use std::cmp;
+
 impl Solution {
     pub fn h_index(citations: Vec<i32>) -> i32 {
-        let mut max = 0;
         let len = citations.len();
-        for i in 0..len{
-            let n = len - i;
+        
+        let mut l = 0;
+        let mut r = len;
+        
+        let mut max = 0;
+        
+        while (l < r){
+            let mid = l + (r - l)/2;
+            let target = len - mid;
             
-            if citations[i] >= n as i32{
-                return n as i32;
+            let citation = citations[mid];
+            if citations[mid] > target as i32{
+                r = mid;
+                max = cmp::max(max, target as i32);
+            }else if citations[mid] < target as i32{
+                l = mid + 1;
+            }else{
+                return citations[mid]
             }
         }
         
-        0
+        max
     }
 }
