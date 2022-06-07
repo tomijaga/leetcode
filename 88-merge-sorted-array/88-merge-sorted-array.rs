8b1f81bd82948dc9f8e3a0e9b56329ab7632162d
Tomi Jaga <jagatomi12@gmail.com>
2022-06-07 00:08:18 -0400

    Time: 2 ms (33.10%), Space: 2 MB (80.99%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.rs
+++ 88-merge-sorted-array/88-merge-sorted-array.rs
@@ -3,20 +3,20 @@ impl Solution {
         let (m, n) = (m as usize, n as usize);
         let mut op = m + n - 1;
         
-        let (mut i, mut j) = ((m - 1) as i32, (n - 1) as i32);
+        let (mut i, mut j) = (m - 1, n - 1);
         
         for ni in (0..nums1.len()).rev(){
-            if (i < 0){
-                nums1[ni] = nums2[j as usize];
+            if (i == usize::MAX ){
+                nums1[ni] = nums2[j];
                 j-=1;
-            }else if (j< 0){
-                nums1[ni] = nums1[i as usize];
+            }else if (j == usize::MAX ){
+                nums1[ni] = nums1[i];
                 i-=1;
-            } else if (nums1[i as usize] > nums2[j as usize]){
-                nums1[ni] = nums1[i as usize];
+            } else if (nums1[i] > nums2[j]){
+                nums1[ni] = nums1[i];
                 i-=1;
             }else{
-                nums1[ni] = nums2[j as usize];
+                nums1[ni] = nums2[j];
                 j-=1;
             }
         }
