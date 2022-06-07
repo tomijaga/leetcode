0b0dcd480e12d5717bca37df7b5751798097229f
Tomi Jaga <jagatomi12@gmail.com>
2022-06-07 10:47:26 -0400

    Time: 38 ms (40.00%), Space: 3 MB (100.00%) - LeetHub

--- 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.rs
+++ 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.rs
@@ -2,26 +2,24 @@ use std::cmp;
 
 impl Solution {
     pub fn minimum_deletions(nums: Vec<i32>) -> i32 {
-        let mut min = (0 , i32::MAX);
-        let mut max = (0 , i32::MIN);
+        let mut min = 0;
+        let mut max = 0;
         
         for (i, &n) in nums.iter().enumerate(){
-            if n < min.1{
-                min.0 = i;
-                min.1 = n;
+            if n < nums[min]{
+                min = i;
             }
             
-            if n > max.1{
-                max.0 = i;
-                max.1 = n;
+            if n > nums[max]{
+                max = i;
             }
             
         }
 
         // [-, -, d2, -, -, -, d1, -, -]
         
-        let d1 = cmp::max(max.0 + 1, min.0 + 1);
-        let d2 = cmp::min(max.0 + 1, min.0 + 1);
+        let d1 = cmp::max(max + 1, min + 1);
+        let d2 = cmp::min(max + 1, min + 1);
         
         let a = nums.len() - d2 + 1;
         let b = d1;
