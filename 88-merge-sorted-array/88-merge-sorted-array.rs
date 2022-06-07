75fe886d3a2ea2ad6cf25c8c0aeb343657128384
Tomi Jaga <jagatomi12@gmail.com>
2022-06-07 00:06:31 -0400

    Time: 2 ms (33.10%), Space: 2.2 MB (16.20%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.rs
+++ 88-merge-sorted-array/88-merge-sorted-array.rs
@@ -1,25 +1,23 @@
 impl Solution {
     pub fn merge(nums1: &mut Vec<i32>, m: i32, nums2: &mut Vec<i32>, n: i32) {
         let (m, n) = (m as usize, n as usize);
-        let op = m + n;
+        let mut op = m + n - 1;
         
-        let (mut i, mut j) = (0, 0);
+        let (mut i, mut j) = ((m - 1) as i32, (n - 1) as i32);
         
-        let n1_copy = nums1[0..m].to_vec();
-        
-        for num in nums1.iter_mut(){
-            if (i >= m){
-                *num = nums2[j];
-                j+=1;
-            }else if (j >=n){
-               *num = n1_copy[i];
-                i+=1;
-            } else if (n1_copy[i]< nums2[j]){
-                *num = n1_copy[i];
-                i+=1;
+        for ni in (0..nums1.len()).rev(){
+            if (i < 0){
+                nums1[ni] = nums2[j as usize];
+                j-=1;
+            }else if (j< 0){
+                nums1[ni] = nums1[i as usize];
+                i-=1;
+            } else if (nums1[i as usize] > nums2[j as usize]){
+                nums1[ni] = nums1[i as usize];
+                i-=1;
             }else{
-                *num = nums2[j];
-                j+=1;
+                nums1[ni] = nums2[j as usize];
+                j-=1;
             }
         }
     }
