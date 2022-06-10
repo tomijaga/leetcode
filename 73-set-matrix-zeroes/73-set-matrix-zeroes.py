85c43a19ad97c1f791e45e25951b98bb6f4b1cbc
Tomi Jaga <jagatomi12@gmail.com>
2022-06-10 01:48:25 -0400

    Time: 198 ms (36.60%), Space: 14.7 MB (89.30%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.py
@@ -2,14 +2,14 @@ class Solution:
     def setZeroes(self, matrix: List[List[int]]) -> None:
         m, n = len(matrix), len(matrix[0])
         
-        rows = []
-        cols = []
+        rows = set()
+        cols = set()
         
         for i in range(0, m):
             for j in range(0, n):
                 if matrix[i][j] == 0:
-                    rows.append(i)
-                    cols.append(j)
+                    rows.add(i)
+                    cols.add(j)
                     
         for i in rows:
             for j in range(0, n):
