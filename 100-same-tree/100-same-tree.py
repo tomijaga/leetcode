8220f8f3d80549d3c962d737e7318d6db2563c8d
Tomi Jaga <jagatomi12@gmail.com>
2022-06-10 01:39:59 -0400

    Time: 41 ms (56.07%), Space: 14 MB (28.33%) - LeetHub

--- 100-same-tree/100-same-tree.py
+++ 100-same-tree/100-same-tree.py
@@ -9,12 +9,10 @@ class Solution:
         if p == None and q == None:
             return True
             
-        if p == None or q == None:
+        if p == None or q == None or p.val != q.val:
             return False
         
-        if p.val == q.val:
-             return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
-        else:
-            return False
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        
         
        
