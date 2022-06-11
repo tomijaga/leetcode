6e3771fc0e793ae74428ee63cd31e11b90f031c8
Tomi Jaga <jagatomi12@gmail.com>
2022-06-11 02:37:10 -0400

    Time: 89 ms (33.33%), Space: 4 MB (100.00%) - LeetHub

--- 1508-range-sum-of-sorted-subarray-sums/1508-range-sum-of-sorted-subarray-sums.rs
+++ 1508-range-sum-of-sorted-subarray-sums/1508-range-sum-of-sorted-subarray-sums.rs
@@ -1,4 +1,7 @@
-use std::collections::BinaryHeap;
+use std::{
+    cmp::Reverse,
+    collections::BinaryHeap
+};
 
 impl Solution {
     pub fn range_sum(mut nums: Vec<i32>, n: i32, left: i32, right: i32) -> i32 {
@@ -17,26 +20,30 @@ impl Solution {
         nums.push(prev + *nums.last().unwrap());
         
         let mut heap = BinaryHeap::new();
+        let mut lower_heap = BinaryHeap::new();
         
+        let mut sum: i64 = 0;
         for i in 0..nums.len(){
             for j in i+1..nums.len(){
-                heap.push(nums[j] - nums[i]);
+                let n = nums[j] - nums[i];
                 
-                if heap.len() > right{
-                    heap.pop();
+                lower_heap.push(n);
+                
+                if lower_heap.len() >= left {
+                    let n = lower_heap.pop().unwrap();
+                    
+                    heap.push(n);
+                    sum+=n as i64;
+                }
+                
+                if heap.len() > right - left + 1{
+                    sum -= heap.pop().unwrap() as i64;
                 }
             }
         }
         
-        // println!("{:?}", &heap);
-//         println!("{:?}", heap.peek());
-//         println!("{:?}", heap.pop());
-//         println!("{:?}", heap.peek());
-        
-        let mut sum: i64 = 0;
-        for _ in left..=right{
-            sum += heap.pop().unwrap() as i64;
-        }
+        // println!("{:?}", &lower_heap); 
+        // println!("{:?}", &heap); 
         
         (sum % (10_i64.pow(9) + 7)) as i32
     }
