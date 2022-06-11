52b52a79ba82e4990e4bc9ef4569975efd0eab41
Tomi Jaga <jagatomi12@gmail.com>
2022-06-11 14:29:37 -0400

    Time: 134 ms (100.00%), Space: 2.4 MB (100.00%) - LeetHub

--- 2301-match-substring-after-replacement/2301-match-substring-after-replacement.rs
+++ 2301-match-substring-after-replacement/2301-match-substring-after-replacement.rs
@@ -1,11 +1,9 @@
 use std::{
-    cmp::max,
     collections::{
         HashMap,
         HashSet
     }
 };
-
 
 impl Solution {
     pub fn match_replacement(s: String, sub: String, mappings: Vec<Vec<char>>) -> bool {
@@ -15,25 +13,22 @@ impl Solution {
             map.entry(v[0]).or_insert(HashSet::new()).insert(v[1]);
         }
         
-        // println!("{:?}", &map);
-        
-        
-        let mut points = 0;
+        let mut matches = 0;
         let mut start = 0;
         
         let mut s = s.chars().collect::<Vec<char>>();
         let mut sub = sub.chars().collect::<Vec<char>>();
         
         for i in 0..=(s.len() - sub.len()){
-            let tmp_points = get_points(&s, &sub, i);
+            let tmp = get_matches(&s, &sub, i);
             
-            if tmp_points > points{
-                points = tmp_points;
+            if tmp > matches{
+                matches = tmp;
                 start = i;
             }
         }
         
-        if points ==  sub.len() as i32{
+        if matches ==  sub.len() as i32{
             return true;
         }
         
@@ -43,27 +38,27 @@ impl Solution {
                 
                 if let Some(&c) = set.get(&s[start + i]){
                     sub[i] = c;
-                    // println!("{:?}", sub[i]);
                 }
             }
         }
         
-        get_points(&s, &sub, start) == sub.len() as i32
+        get_matches(&s, &sub, start) == sub.len() as i32
     }
 }
 
-pub fn get_points(s: &Vec<char>, sub: &Vec<char>, start: usize)-> i32{
+// number of matching chars from the start index
+pub fn get_matches(s: &Vec<char>, sub: &Vec<char>, start: usize)-> i32{
     let mut s_chars = s.iter().skip(start);
     let mut sub_chars = sub.iter();
     
-    let mut points = 0;
+    let mut matches = 0;
     for sub_char in sub_chars{
         let s_char = s_chars.next().unwrap();
         
         if s_char == sub_char{
-            points +=1;
+            matches +=1;
         }
     }
     
-    points
+    matches
 }
