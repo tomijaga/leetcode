a2d6cf7bb0a65f50726b41d0d395f23b9e246f48
Tomi Jaga <jagatomi12@gmail.com>
2022-06-13 19:04:36 -0400

    Time: 101 ms (47.06%), Space: 5.3 MB (100.00%) - LeetHub

---
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.rs
@@ -0,0 +1,23 @@
+
+impl Solution {
+    pub fn rearrange_array(nums: Vec<i32>) -> Vec<i32> {
+        let mut nums = nums;
+        let mut res = vec![0; nums.len()];
+        
+        let mut pos = 0;
+        let mut neg = 1;
+        
+        for n in nums{
+            
+            if n >=0{
+                res[pos] = n;
+                pos +=2;
+            }else{
+                res[neg] = n;
+                neg +=2;
+            }
+        }
+        
+        res
+    }
+}
