dae1f4613913fad9217a5baa5fad9dffccffd2e2
Tomi Jaga <jagatomi12@gmail.com>
2022-06-13 19:06:08 -0400

    Time: 99 ms (47.06%), Space: 5.4 MB (52.94%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.rs
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.rs
@@ -1,7 +1,6 @@
 
 impl Solution {
     pub fn rearrange_array(nums: Vec<i32>) -> Vec<i32> {
-        let mut nums = nums;
         let mut res = vec![0; nums.len()];
         
         let mut pos = 0;
