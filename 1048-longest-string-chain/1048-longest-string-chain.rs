ffb339c870d067526f53a6994c9abf92f4a357f0
Tomi Jaga <jagatomi12@gmail.com>
2022-06-14 23:21:50 -0400

    Time: 26 ms (50.00%), Space: 2.3 MB (100.00%) - LeetHub

---
+++ 1048-longest-string-chain/1048-longest-string-chain.rs
@@ -0,0 +1,37 @@
+use std::{
+    cmp::max,
+    collections::HashMap
+};
+
+impl Solution {
+    pub fn longest_str_chain(mut words: Vec<String>) -> i32 {
+        let mut map = HashMap::new();
+        words.sort_unstable_by(|a, b|{a.len().cmp(&b.len())});
+        
+        for word in words{
+            let bytes = word.as_bytes().to_vec();
+            let mut chain = 0;
+            
+            for i in 0..bytes.len(){
+                let mut successor = bytes.clone();
+                successor.remove(i);
+                
+                if let Some(&cnt) = map.get(&successor){
+                    chain = max(cnt, chain);
+                }
+            }
+            
+            map.insert(bytes, chain + 1);
+            
+        }
+
+        let (_, max_cnt) = map
+        .into_iter()
+        .max_by(|(_, n), (_, n1)|{
+            n.cmp(&n1)
+        })
+        .unwrap();
+        
+        max_cnt
+    }
+}
