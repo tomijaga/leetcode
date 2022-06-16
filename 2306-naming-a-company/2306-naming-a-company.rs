855442f2f48a102d5bb14b4452d5c187c5368760
Tomi Jaga <jagatomi12@gmail.com>
2022-06-16 19:37:22 -0400

    Time: 263 ms (77.78%), Space: 7.2 MB (66.67%) - LeetHub

--- 2306-naming-a-company/2306-naming-a-company.rs
+++ 2306-naming-a-company/2306-naming-a-company.rs
@@ -1,27 +1,25 @@
-use std::collections::{HashMap, HashSet};
+
+use std::collections::HashSet;
 
 impl Solution {
     pub fn distinct_names(ideas: Vec<String>) -> i64 {
-        let mut map: HashMap<u8, HashSet<&str>> = HashMap::new();
+        let mut sets =  vec![HashSet::new(); 26];
         
-        for idea in ideas.iter() {
-            let first_char = idea.as_bytes()[0];
-            map.entry(first_char).or_insert(HashSet::new()).insert(&idea[1..]);
+        for idea in ideas.iter(){
+            let bytes = idea.as_bytes();
+            sets[(bytes[0] - b'a') as usize].insert(&idea[1..]);
         }
         
         let mut res = 0;
-        for i in b'a'..=b'z' {
-            for j in i+1..=b'z' {
-                if !map.contains_key(&i) || !map.contains_key(&j) {
-                    continue;
-                }
-                let mut set_a = map.get(&i).unwrap();
-                let mut set_b = map.get(&j).unwrap();
+        for i in 0..sets.len() - 1{
+            for j in (i+1)..sets.len(){
+                let (a, b) = (&sets[i], &sets[j]);
                 
-                res += 2 * (set_a - set_b).len() * (set_b - set_a).len();
-            }
+                let duplicates = a.intersection(b).collect::<HashSet<_>>().len();
+                res += 2_i64 * ((a.len() - duplicates) * (b.len() - duplicates)) as i64;
+            }   
         }
         
-        res as i64
+        res
     }
 }
