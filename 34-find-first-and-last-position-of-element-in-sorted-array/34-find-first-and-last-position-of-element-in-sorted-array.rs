6de38ce62e4fc3afa86cab281c47779ceaa51730
Tomi Jaga <jagatomi12@gmail.com>
2022-06-16 07:30:20 -0400

    Time: 0 ms (100.00%), Space: 2.3 MB (100.00%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.rs
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.rs
@@ -1,10 +1,9 @@
 impl Solution {
     pub fn search_range(nums: Vec<i32>, target: i32) -> Vec<i32> {
-        if nums.len() == 0 || target < nums[0]{
+        if nums.len() == 0{
             return vec![-1, -1];
         }
         
-        // binary search for left index
         let (mut l, mut r) = (0, nums.len() -1);
         while l < r{
             let mid = l + (r - l)/2;
@@ -12,15 +11,35 @@ impl Solution {
             
             if n < target{
                 l = mid + 1;
+            }else if n > target{
+                r = mid;
+            }else{
+                r = mid;
+                break;
+            }
+        }
+        
+        if nums[r] != target {
+            return vec![-1, -1];
+        }
+        
+        let mut target_index = r;
+        let (mut l, mut r) = (0, target_index);
+        
+        while (l < r){
+            let mid = l + (r - l)/2;
+            let n = nums[mid];
+
+            if n < target{
+                l = mid + 1;
             }else{
                 r = mid;
             }
         }
+
+        let left = r;
+        let (mut l, mut r) = (target_index, nums.len() - 1);
         
-        let left = l;
-        
-        // binary search for the right index
-        r = nums.len() - 1;
         while (l <= r){
             let mid = (l + r)/2;
             let n = nums[mid];
@@ -29,14 +48,10 @@ impl Solution {
                 r = mid - 1;
             }else{
                 l = mid + 1;
+                
             }
         }
         
-        if nums[r] == target && nums[left] == target {
-            vec![left as i32, r as i32]
-        }else{
-            vec![-1, -1]
-        }
-        
+        vec![left as i32, r as i32]
     }
 }
