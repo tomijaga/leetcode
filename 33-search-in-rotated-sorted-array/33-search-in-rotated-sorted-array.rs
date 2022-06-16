a87e543af4461d35b6ea2622a499c7b36829503b
Tomi Jaga <jagatomi12@gmail.com>
2022-06-16 05:39:39 -0400

    Time: 0 ms (100.00%), Space: 2.1 MB (45.19%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.rs
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.rs
@@ -1,33 +1,31 @@
 impl Solution {
     pub fn search(nums: Vec<i32>, target: i32) -> i32 {
         let (mut l, mut r) = (0, nums.len() - 1);
-        let mut start = nums[0];
-        let mut min = start;
+        let mut first = nums[0];
         
-        while l < r{
+        while l <= r{
             let mid = l + (r-l)/2;
-            
             let n = nums[mid];
             
-            if n < min{
-                min = n;
-                r = mid;
+            if target < first{
+                if n < target || n >= first{
+                    l = mid +1;
+                }else if n > target{
+                    r = mid - 1;
+                }else{
+                    return mid as i32;
+                }
+            }else if target > first{
+                if n > target || n < first{
+                    r = mid - 1;
+                }else if n < target{
+                    l = mid + 1;
+                }else{
+                    return mid as i32;
+                }
             }else{
-                l = mid + 1;
+                return 0;
             }
-        }
-        
-        let mut partition = r;
-        // println!("{:?}", partition);
-        let a = nums[..partition].binary_search(&target);
-        let b = nums[partition..].binary_search(&target);
-        
-        if a.is_ok(){
-            return a.unwrap() as i32;
-        }
-        
-         if b.is_ok(){
-            return (b.unwrap() + partition) as i32;
         }
         
         -1
