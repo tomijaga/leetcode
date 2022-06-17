77848028653d3218af1678dcc6aebee340147730
Tomi Jaga <jagatomi12@gmail.com>
2022-06-17 00:51:51 -0400

    Time: 0 ms (100.00%), Space: 2.2 MB (100.00%) - LeetHub

--- 968-binary-tree-cameras/968-binary-tree-cameras.rs
+++ 968-binary-tree-cameras/968-binary-tree-cameras.rs
@@ -2,10 +2,17 @@ use std::rc::Rc;
 use std::cell::RefCell;
 use std::cmp::min;
 
+#[derive(Eq, PartialEq)]
+pub enum NodeMsg{
+    Leaf,
+    Camera,
+    Skip    
+}
+
 impl Solution {
     pub fn min_camera_cover(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
         
-        pub fn cnt_camera(node: &TreeNode, res: &mut i32) -> i32 {
+        pub fn cnt_camera(node: &TreeNode, res: &mut i32) -> NodeMsg {
             let (mut left_cnt, mut right_cnt) = (0, 0);
             let has_child = false;
             
@@ -13,25 +20,25 @@ impl Solution {
                 let l = left.borrow();
                 cnt_camera(&l, res)
             }else{
-                2
+                NodeMsg::Skip
             };
             
             let r = if let Some(ref right) = node.right{
                 let r = right.borrow();
                 cnt_camera(&r, res)
             }else{
-                2
+                NodeMsg::Skip
             };
             
             
-            if l == 0 || r == 0{
+            if l == NodeMsg::Leaf || r == NodeMsg::Leaf{
                 *res +=1;
-                1
+                NodeMsg::Camera
             }else{
-                if (l == 1 || r == 1) {
-                    2
+                if (l == NodeMsg::Camera || r == NodeMsg::Camera) {
+                    NodeMsg::Skip
                 }else{
-                    0
+                    NodeMsg::Leaf
                 }
             }
         }
@@ -40,7 +47,7 @@ impl Solution {
         let mut res = 0;
         if let Some(ref node) = root {
             let node_msg = cnt_camera(&node.borrow(), &mut res);
-            (if node_msg == 0{
+            (if node_msg == NodeMsg::Leaf{
                 1
             }else{
                 0
