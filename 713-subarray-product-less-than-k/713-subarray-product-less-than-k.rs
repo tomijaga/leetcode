b38bee672c5137a35865bd658747ffeda5db0928
Tomi Jaga <jagatomi12@gmail.com>
2022-06-20 05:55:31 -0400

    Time: 18 ms (69.70%), Space: 2.4 MB (36.36%) - LeetHub

--- 713-subarray-product-less-than-k/713-subarray-product-less-than-k.rs
+++ 713-subarray-product-less-than-k/713-subarray-product-less-than-k.rs
@@ -1,34 +1,18 @@
 impl Solution {
     pub fn num_subarray_product_less_than_k(nums: Vec<i32>, k: i32) -> i32 {
-
-        let mut subs = 0;
+        let (mut left, mut right, mut count, mut product) = (0, 0, 0, 1);
         
-        let mut i = 0;
-        let mut j = 1;
-        
-        let mut prod = nums[i];
-        
-        while i< j && j <= nums.len(){
-            let n = nums[j - 1];
+        for right in 0..nums.len() {
+            product *= nums[right];
             
-            if prod >= k{
-                prod /= nums[i];
-                i+=1;
-                if i==j && i < nums.len(){
-                    j+=1;
-                    prod = nums[i];
-                }
-                
-            }else if prod < k{
-                subs+= (j - i) as i32;
-                j+=1;
-                
-                if j<= nums.len(){
-                    prod *= nums[j - 1];
-                }
+            while product >= k && left <= right {
+                product /= nums[left];
+                left += 1;
             }
+            
+            count += right - left + 1;
         }
         
-        subs
+        count as i32
     }
 }
