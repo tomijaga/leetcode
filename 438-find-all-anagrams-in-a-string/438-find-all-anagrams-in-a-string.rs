9294bb4e399f54f3eb258641d16caba0b19ce80c
Tomi Jaga <jagatomi12@gmail.com>
2022-06-20 05:03:08 -0400

    Time: 30 ms (23.81%), Space: 2.3 MB (66.67%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.rs
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.rs
@@ -1,24 +1,51 @@
+use std::collections::HashMap;
+
 impl Solution {
     pub fn find_anagrams(s: String, mut p: String) -> Vec<i32> {
         if p.len() > s.len() {
             return vec![];
         }
         
-        let mut p : Vec<char> = p.chars().collect();
-        p.sort_unstable();
+        let mut s = s.chars().collect::<Vec<char>>();
         
         let mut res = vec![];
         
+        let mut sMap = HashMap::new();
+        let mut pMap = HashMap::new();
+        
+        for (a, &b) in p.chars().zip(s.iter()){
+            *pMap.entry(a).or_insert(0) +=1;
+            *sMap.entry(b).or_insert(0) +=1;
+        }
+        
+        // println!("{:?}", (&sMap, &pMap));
+        
         for i in 0..=(s.len() - p.len()){
-            let mut slice = (&s[i..(i + p.len())]).chars().collect::<Vec<char>>();
-            slice.sort_unstable();
-            
-            if slice == p{
-                // println!("{:?}", (slice, &p));
+            if is_anagram(&sMap, &pMap){
                 res.push(i as i32);
+            }
+            
+            *sMap.get_mut(&s[i]).unwrap()-=1;
+            if i + p.len() < s.len(){
+                *sMap.entry(s[i + p.len()]).or_insert(0) +=1;
             }
         }
         
         res
     }
+}
+
+pub fn is_anagram(sMap: &HashMap<char, i32>, pMap: &HashMap<char, i32>) -> bool{
+    for (c, cnt) in pMap{
+        
+        if let Some (cnt2) = sMap.get(&c){
+            if cnt != cnt2{
+                return false;
+            }
+        }else{
+            return false;
+        }
+    }
+    
+    return true;
 }
