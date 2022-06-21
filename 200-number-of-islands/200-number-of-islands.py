b65827e55a7b733d63ffc356a398128821a93ef2
Tomi Jaga <jagatomi12@gmail.com>
2022-06-21 08:23:03 -0400

    Time: 638 ms (13.31%), Space: 16.4 MB (63.44%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -9,29 +9,29 @@ class Solution:
         
         for i in range(0, len(grid)):
             for j in range(0, len(grid[0])):
-                num += self.dfs(grid, visited, q, (i, j))
-                # print("------")
+                if grid[i][j] == '1':
+                    num += self.dfs(grid, (i, j))
+                    # print("------")
         return num
                 
-    def dfs(self, grid: List[List[str]], visited, q, point: (int, int)) -> bool:
+    def dfs(self, grid: List[List[str]], point: (int, int)) -> bool:
         (m, n) = (len(grid), len(grid[0]))
         (i, j) = point
         
         if i < 0 or j < 0 or i >= m or j >= n:
             return False
         
-        if point in visited or grid[i][j] == '0':
+        if grid[i][j] != '1':
             return False
         
-        # print(True, (i, j))
-        visited.add(point)
+        print(True, (i, j))
         
-        q.append((i - 1, j))
-        q.append((i + 1, j))
-        q.append((i, j - 1))
-        q.append((i, j + 1))
+        # mark as visited
+        grid[i][j] = '#'
         
-        while len(q) > 0:
-            self.dfs(grid, visited, q, q.popleft())
-        
+        self.dfs(grid, (i - 1, j))
+        self.dfs(grid, (i + 1, j))
+        self.dfs(grid, (i, j - 1))
+        self.dfs(grid, (i, j + 1))
+
         return True
