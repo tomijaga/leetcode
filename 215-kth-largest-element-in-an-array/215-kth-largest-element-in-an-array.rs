99947ed3d78f00d78f695c0cdd1266dac8e6f160
Tomi Jaga <jagatomi12@gmail.com>
2022-06-21 21:58:27 -0400

    Time: 0 ms (100.00%), Space: 2.2 MB (42.27%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.rs
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.rs
@@ -2,23 +2,21 @@ use std::collections::BinaryHeap;
 use std::cmp::Reverse;
 
 impl Solution {
-    pub fn find_kth_largest(mut nums: Vec<i32>, k: i32) -> i32 {
-//         let mut heap = BinaryHeap::from(nums);
+    pub fn find_kth_largest(nums: Vec<i32>, k: i32) -> i32 {
+        let mut heap = BinaryHeap::new();
         
-//         for n in nums{
-//             heap.push(Reverse(n));
-//             if heap.len() > k as usize{
-//                 heap.pop();
-//             }
-//         }
+        for n in nums{
+            heap.push(Reverse(n));
+            if heap.len() > k as usize{
+                heap.pop();
+            }
+        }
         
-//         if let Some(Reverse(kth_largest)) = heap.pop(){
-//             kth_largest
-//         }else{
-//             0
-//         }
+        if let Some(Reverse(kth_largest)) = heap.pop(){
+            kth_largest
+        }else{
+            0
+        }
         
-        nums.sort_unstable();
-        nums[nums.len() - k as usize]
     }
 }
