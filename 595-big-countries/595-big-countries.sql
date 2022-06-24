22ef4c3e3df086e2d655b2fdd8f39dc1a7db111d
Tomi Jaga <jagatomi12@gmail.com>
2022-06-24 07:42:21 -0400

    Time: 282 ms (70.49%), Space: 0B (100.00%) - LeetHub

--- 595-big-countries/595-big-countries.sql
+++ 595-big-countries/595-big-countries.sql
@@ -1,10 +1,4 @@
 # Write your MySQL query statement below
 select name, area, population
 from world
-where area >= 3000000
-
-union
-
-select name, area, population
-from world
-where population >= 25000000
+where area >= 3000000 or population >= 25000000
