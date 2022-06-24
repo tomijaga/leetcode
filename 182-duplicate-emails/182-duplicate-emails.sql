cc1d027fc0377b5a203c9cb32c952f1d72bbfc01
Tomi Jaga <jagatomi12@gmail.com>
2022-06-24 19:57:34 -0400

    Time: 351 ms (70.76%), Space: 0B (100.00%) - LeetHub

--- 182-duplicate-emails/182-duplicate-emails.sql
+++ 182-duplicate-emails/182-duplicate-emails.sql
@@ -1,5 +1,4 @@
 # Write your MySQL query statement below
-select Email
-from Person
-group by Email
-having count(*) > 1
+select distinct p1.email
+from person p1, person p2
+where p1.id <> p2.id and p1.email like p2.email
