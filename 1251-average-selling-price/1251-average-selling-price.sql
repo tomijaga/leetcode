e8c1a0a8d248632f09624636bd7ff4888ab9fd64
Tomi Jaga <jagatomi12@gmail.com>
2022-06-25 10:12:12 -0400

    Time: 1828 ms (5.02%), Space: 0B (100.00%) - LeetHub

--- 1251-average-selling-price/1251-average-selling-price.sql
+++ 1251-average-selling-price/1251-average-selling-price.sql
@@ -3,6 +3,6 @@ select
     round(sum(p.price * u.units) / sum(u.units), 2)  as average_price
 from prices p, unitsSold u
 where p.product_id = u.product_id
-    and u.purchase_date <= p.end_date
-    and u.purchase_date >= p.start_date
+    and u.purchase_date between p.start_date 
+    and p.end_date
 group by product_id
