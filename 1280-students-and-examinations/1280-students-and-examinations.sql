375b1052ececf1264c5cab91806093cd11fa5282
Tomi Jaga <jagatomi12@gmail.com>
2022-06-25 16:51:49 -0400

    Time: 884 ms (42.14%), Space: 0B (100.00%) - LeetHub

---
+++ 1280-students-and-examinations/1280-students-and-examinations.sql
@@ -0,0 +1,15 @@
+select s.student_id, s.student_name, sj.subject_name, ifnull(cnt, 0) as attended_exams
+from students s
+cross join subjects sj
+left join
+   (select student_id, subject_name, count(*) as cnt
+    from examinations
+    group by student_id, subject_name) e
+on s.student_id = e.student_id and sj.subject_name = e.subject_name
+order by student_id, subject_name
+# left join subjects sj
+# on sj.subject_name = e.subject_name
+
+# select s.student_id, s.student_name, sj.subject_name
+# from students s
+# join subjects sj
