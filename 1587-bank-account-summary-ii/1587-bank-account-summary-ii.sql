b96212c946a54f628ff93f0e0816b864e42d1fa5
Tomi Jaga <jagatomi12@gmail.com>
2022-06-24 20:09:23 -0400

    Time: 531 ms (96.79%), Space: 0B (100.00%) - LeetHub

--- 1587-bank-account-summary-ii/1587-bank-account-summary-ii.sql
+++ 1587-bank-account-summary-ii/1587-bank-account-summary-ii.sql
@@ -4,7 +4,7 @@ from
     (select account, sum(amount) as amount
     from transactions
     group by account
-    having sum(amount) > 10000
+    having amount > 10000
     ) r
 left join users u
 on r.account = u.account
