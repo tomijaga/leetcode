7df193545bc3d3a18f8022ba17dc764b60d3c94d
Tomi Jaga <jagatomi12@gmail.com>
2022-06-25 10:25:55 -0400

    Time: 963 ms (16.13%), Space: 0B (100.00%) - LeetHub

--- 1571-warehouse-manager/1571-warehouse-manager.sql
+++ 1571-warehouse-manager/1571-warehouse-manager.sql
@@ -1,9 +1,7 @@
 select 
     name as warehouse_name,
-    sum(units * p.volume) as volume
+    sum(units * p.height * p.length * p.width) as volume
 from warehouse w
-left join 
-    (select product_id, (width * length * height) as volume
-    from products) p
+left join products p
 on w.product_id = p.product_id
 group by name
