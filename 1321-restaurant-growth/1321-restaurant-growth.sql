ea6240e5d4822e1624256c9ad480e2ef23bfdcb0
Tomi Jaga <jagatomi12@gmail.com>
2022-06-26 06:15:44 -0400

    Time: 1149 ms (5.04%), Space: 0B (100.00%) - LeetHub

--- 1321-restaurant-growth/1321-restaurant-growth.sql
+++ 1321-restaurant-growth/1321-restaurant-growth.sql
@@ -1,13 +1,17 @@
-
+with const as 
+    (select 
+        visited_on, 
+        sum(amount) as amount 
+     from customer 
+     group by visited_on)
 select 
-    visited_on, 
-    (select sum(amount)
-     from customer 
-     where to_days(c.visited_on) - to_days(visited_on) between 0 and 6  ) as amount, 
-    (select round(sum(amount)/7, 2) 
-     from customer 
-     where to_days(c.visited_on) - to_days(visited_on) between 0 and 6   ) as average_amount
-from customer c
-where to_days(visited_on) - 6 in (select to_days(visited_on) from customer)
+    b.visited_on, 
+    sum(a.amount) as amount, 
+    round(avg(a.amount), 2) as average_amount
+from 
+    const a,
+    const b
+where to_days(b.visited_on) - to_days(a.visited_on) between 0 and 6
 group by visited_on
+having count(*) = 7
 order by visited_on
