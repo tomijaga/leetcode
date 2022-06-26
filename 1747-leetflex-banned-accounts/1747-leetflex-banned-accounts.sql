8eff0c6daec315e9ffe93730a98ec66202d2abb7
Tomi Jaga <jagatomi12@gmail.com>
2022-06-25 21:07:39 -0400

    Time: 461 ms (88.14%), Space: 0B (100.00%) - LeetHub

--- 1747-leetflex-banned-accounts/1747-leetflex-banned-accounts.sql
+++ 1747-leetflex-banned-accounts/1747-leetflex-banned-accounts.sql
@@ -2,5 +2,5 @@ select distinct l1.account_id
 from loginfo l1, loginfo l2
 where l1.account_id = l2.account_id
     and l1.ip_address <> l2.ip_address
-    and l2.login <= l1.logout
-    and l1.login <= l2.logout
+    # and l2.login <= l1.logout
+    and l1.login between l2.login and l2.logout
