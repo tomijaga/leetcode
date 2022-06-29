6cf4d7b0ee2a59b4e9e4bd84d6502e6da545196e
Tomi Jaga <jagatomi12@gmail.com>
2022-06-29 12:44:52 -0400

    Time: 9 ms (50.00%), Space: 2.5 MB (50.00%) - LeetHub

---
+++ 406-queue-reconstruction-by-height/406-queue-reconstruction-by-height.py
@@ -0,0 +1,22 @@
+use std::cmp::Reverse;
+
+impl Solution {
+    pub fn reconstruct_queue(mut people: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
+        people.sort_unstable_by(|a, b|{
+            if b[0] == a[0]{
+                (a[1]).cmp(&b[1])
+            }else{
+                (b[0]).cmp(&a[0])
+            }
+        });
+        
+        // println!("{:?}", &people);
+        let mut q = vec![];
+        
+        for v in people{
+            q.insert(v[1] as usize, v);
+        }
+        
+        q
+    }
+}
