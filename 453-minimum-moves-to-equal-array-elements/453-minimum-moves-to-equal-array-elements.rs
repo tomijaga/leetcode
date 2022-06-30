5042c9ea85d7265a2c9e0fd468830895484ccfad
Tomi Jaga <jagatomi12@gmail.com>
2022-06-30 09:59:13 -0400

    Time: 9 ms (40.00%), Space: 2 MB (100.00%) - LeetHub

--- 453-minimum-moves-to-equal-array-elements/453-minimum-moves-to-equal-array-elements.rs
+++ 453-minimum-moves-to-equal-array-elements/453-minimum-moves-to-equal-array-elements.rs
@@ -1,10 +1,5 @@
 impl Solution {
     pub fn min_moves(nums: Vec<i32>) -> i32 {
-        let min = nums.iter().min().unwrap();
-        
-        let sum: i32 = nums.iter().sum();
-        let min_sum = (min * nums.len() as i32);
-        
-        sum - min_sum
+        nums.iter().sum::<i32>() - (nums.iter().min().unwrap() * nums.len() as i32)
     }
 }
