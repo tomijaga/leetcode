63258ac03612912d4fca7b0a51ad1a02ce0ac6f0
Tomi Jaga <jagatomi12@gmail.com>
2022-06-30 13:31:45 -0400

    Time: 1237 ms (11.76%), Space: 11.2 MB (11.76%) - LeetHub

---
+++ 1197-minimum-knight-moves/1197-minimum-knight-moves.rs
@@ -0,0 +1,55 @@
+use std::collections::{HashSet, VecDeque};
+
+type Point = (i32,i32);
+
+impl Solution {
+    pub fn min_knight_moves(x: i32, y: i32) -> i32 {
+        let mut visited = HashSet::new();
+        let mut q = VecDeque::from([(0, 0)]);
+        
+        let len = 8;
+        let mut n = 0;
+        
+        while !q.is_empty(){
+            
+            for _ in 0..q.len(){
+                
+                let (i, j) = q.pop_front().unwrap();
+            
+                if (i, j) == (x, y){
+                    return n;
+                }
+                
+                bfs(&mut visited, &mut q, i, j);
+            }
+            
+            n+=1;
+        }
+        
+        unreachable!()
+    }
+}
+
+pub fn bfs(visited: &mut HashSet<Point>,q: &mut VecDeque<Point>, i: i32, j:i32){
+    
+    if !visited.contains(&(i, j)){
+        
+        visited.insert((i,j));
+        
+        let opts = [
+            (i + 2, j - 1),
+            (i + 2, j + 1),
+            (i - 2, j - 1),
+            (i - 2, j + 1),
+            (i + 1, j - 2),
+            (i + 1, j + 2),
+            (i - 1, j - 2),
+            (i - 1, j + 2)
+        ];
+
+        for (x,y) in opts{
+            q.push_back((x, y));
+        }
+        
+    }
+}
