fc1f0d4549b29bbae0784b61ad16b27350723dbc
Tomi Jaga <jagatomi12@gmail.com>
2022-06-30 10:39:19 -0400

    Time: 2 ms (62.50%), Space: 2.4 MB (12.50%) - LeetHub

--- 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.rs
+++ 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.rs
@@ -1,8 +1,17 @@
 impl Solution {
     pub fn min_moves2(mut nums: Vec<i32>) -> i32 {
         nums.sort_unstable();
-        let mid = nums[nums.len()/2];
         
-        nums.into_iter().map(|n|{ (n - mid).abs() }).sum()
+        let (mut i, mut j) = (0, nums.len() - 1);
+        
+        let mut d = 0;
+        
+        while i < j{
+            d += (nums[j] - nums[i]);
+            i+=1;
+            j-=1;
+        }
+        
+        d
     }
 }
