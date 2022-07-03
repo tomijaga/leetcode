11f1566bc49936cf6a8fa4645fe73f4ee06b9428
Tomi Jaga <jagatomi12@gmail.com>
2022-07-03 05:44:24 -0400

    Time: 10 ms (8.33%), Space: 2.6 MB (10.42%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.rs
+++ 740-delete-and-earn/740-delete-and-earn.rs
@@ -1,43 +1,45 @@
-use std::collections::HashMap;
+use std::collections::BTreeMap;
 
 impl Solution {
     pub fn delete_and_earn(nums: Vec<i32>) -> i32 {
-        let mut map = HashMap::new();
+        let mut map = BTreeMap::new();
         
         for n in nums{
             *map.entry(n).or_insert(0)+= n;
         }
     
         
-        let mut arr = map.into_iter().collect::<Vec<_>>();
+        let mut memo = vec![0; map.len()];
+        let mut twoPrev = -1;
+        let mut prev = -1;
+        let mut max = 0;
         
-        if arr.len() == 1{
-            return arr[0].1;
-        }
-        
-        arr.sort_unstable_by_key(|a|a.0);
-        
-        // println!("{:?}", &arr);
-        
-        let mut memo = vec![0; arr.len()];
-        memo[0] = arr[0].1;
-        
-        if arr[0].0 + 1 == arr[1].0{
-            memo[1] = (arr[1].1).max(memo[0]);
-        }else{
-            memo[1] = memo[0] + arr[1].1;
-        }
-        
-        for i in 2..arr.len(){
-            if arr[i- 1].0 + 1 == arr[i].0{
-                memo[i] = (memo[i - 2] + arr[i].1).max(memo[i - 1]);
-            }else{
-                memo[i] = memo[i - 1] + arr[i].1;
+        for (i, (&n, &total)) in map.clone().iter().enumerate(){
+            if i == 0{
+                prev = n;
+                continue;
             }
+            
+            if prev + 1 == n{
+                let one = *map.get(&prev).unwrap();
+                let two = if twoPrev > 0{
+                    *map.get(&twoPrev).unwrap()
+                }else{
+                    0
+                };
+                
+                map.insert(n, (two + total).max(one));
+            }else{
+                let one = *map.get(&prev).unwrap();
+                map.insert(n, one + total);
+            }
+            
+            twoPrev = prev;
+            prev = n;
         }
         
-        // println!("{:?}", &memo);
+        // println!("{:?}", &map);
         
-        memo[arr.len() - 1].max(memo[arr.len() - 2])
+        *map.get(&prev).unwrap()
     }
 }
