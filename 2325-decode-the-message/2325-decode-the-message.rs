f374e8ae308aaf16094139ae82293071f9955a3d
Tomi Jaga <jagatomi12@gmail.com>
2022-07-03 00:32:13 -0400

    Time: 1 ms (100.00%), Space: 2.2 MB (100.00%) - LeetHub

--- 2325-decode-the-message/2325-decode-the-message.rs
+++ 2325-decode-the-message/2325-decode-the-message.rs
@@ -1,17 +1,19 @@
+
+
 use std::collections::HashMap;
 
 impl Solution {
     pub fn decode_message(key: String, message: String) -> String {
         let mut i:usize = 0;
         
-        let mut map = HashMap::from([(' ', ' ')]);
+        let mut map = vec!['-'; 26];
         let mut message = message.chars().collect::<Vec<char>>();    
         
         let mut alphabets:Vec<char> = "abcdefghijklmnopqrstuvwxyz".chars().collect();
         
         for c in key.chars(){
-            if c != ' ' && map.get(&c).is_none(){
-                map.insert(c, alphabets[i]);
+            if c != ' ' && map[id(c)] == '-'{
+                map[id(c)] = alphabets[i];
                 i+=1;
             }
             
@@ -21,9 +23,16 @@ impl Solution {
         }
         
         for c in message.iter_mut(){
-            *c = *map.get(c).unwrap();
+            if *c == ' '{
+                continue;
+            }
+            
+            *c = map[id(*c)];
         }
-        
         message.into_iter().collect::<String>()
     }
+}
+
+pub fn id(c: char)-> usize{
+    c as usize - 'a' as usize
 }
