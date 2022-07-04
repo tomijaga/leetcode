39cbae6eee2f6fa0e449bd549d9a98bc323331f3
Tomi Jaga <jagatomi12@gmail.com>
2022-07-04 19:45:08 -0400

    Time: 4 ms (80.00%), Space: 2.4 MB (60.00%) - LeetHub

--- 135-candy/135-candy.rs
+++ 135-candy/135-candy.rs
@@ -1,17 +1,20 @@
 impl Solution {
     pub fn candy(ratings: Vec<i32>) -> i32 {
         let n = ratings.len();
-        let mut ans = vec![1; n];
+        let mut res = vec![1; n];
+        
         for i in 1..n {
             if ratings[i] > ratings[i - 1] {
-                ans[i] = ans[i - 1] + 1;
+                res[i] = res[i - 1] + 1;
             }
         }
-        for i in (0..(n - 1)).rev() {
+        
+        for i in (0..ratings.len() -1).rev() {
             if ratings[i] > ratings[i + 1] {
-                ans[i] = std::cmp::max(ans[i + 1] + 1, ans[i]);
+                res[i] = (res[i + 1] + 1).max(res[i]);
             }
         }
-        ans.into_iter().sum()
+        
+        res.into_iter().sum()
     }
 }
