834529e7c6a6d83ac8dd4870de54584e5a98be24
Tomi Jaga <jagatomi12@gmail.com>
2022-07-04 19:45:32 -0400

    Time: 0 ms (100.00%), Space: 2.4 MB (46.67%) - LeetHub

--- 135-candy/135-candy.rs
+++ 135-candy/135-candy.rs
@@ -1,9 +1,8 @@
 impl Solution {
     pub fn candy(ratings: Vec<i32>) -> i32 {
-        let n = ratings.len();
-        let mut res = vec![1; n];
+        let mut res = vec![1; ratings.len()];
         
-        for i in 1..n {
+        for i in 1..ratings.len() {
             if ratings[i] > ratings[i - 1] {
                 res[i] = res[i - 1] + 1;
             }
