c4a344b22a978f82cbd6ab5fcd87962f11e6f622
Tomi Jaga <jagatomi12@gmail.com>
2022-07-04 20:16:34 -0400

    Time: 1171 ms (77.67%), Space: 28 MB (16.04%) - LeetHub

---
+++ 2256-minimum-average-difference/2256-minimum-average-difference.py
@@ -0,0 +1,25 @@
+from itertools import accumulate
+
+class Solution:
+    def minimumAverageDifference(self, nums: List[int]) -> int:
+        size = len(nums)
+
+        prefix_sum = list(accumulate(nums))
+        total = prefix_sum[-1]
+        
+        print(prefix_sum)
+        min_tuple = [0, sys.maxsize]
+        
+        for (i, n) in enumerate(prefix_sum):
+            i = i + 1
+            avg_i = floor(n/i)
+            
+            diff = size - i
+            total_avg = floor((total - n) / (diff if diff>0 else 1))
+
+            avg = abs( avg_i - total_avg) 
+            if min_tuple[1] > avg:
+                min_tuple[1] = avg
+                min_tuple[0] = i - 1
+            
+        return min_tuple[0]
