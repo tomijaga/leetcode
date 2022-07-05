fa934707c38114b375fb3ff646f38151fddfa760
Tomi Jaga <jagatomi12@gmail.com>
2022-07-04 21:15:26 -0400

    Time: 271 ms (21.28%), Space: 14.6 MB (5.32%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.rs
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.rs
@@ -12,8 +12,12 @@ impl Solution {
         
         while set.len() > 0{
             if let Some(&n) = set.iter().next(){
+                let mut cnt = 1;
+                
                 set.remove(&n);
-                max_cnt = max_cnt.max(dfs(&mut set, 0, n) + 1);
+                dfs(&mut set, &mut (cnt), 0, n);
+                
+                max_cnt = max_cnt.max(cnt);
             }
         }
         
@@ -21,24 +25,23 @@ impl Solution {
     }
 }
 
-pub fn dfs(set: &mut HashSet<i32>, dir: i32, n: i32) -> i32{
-    let mut cnt = 0;
+pub fn dfs(set: &mut HashSet<i32>, cnt: &mut i32, dir: i32, n: i32){
     
     if dir != 2{
         let new_n = n+1;
         if set.contains(&new_n){
-            cnt += dfs(set, 1, new_n) + 1;
+            *cnt += 1;
             set.remove(&new_n);
+            dfs(set, cnt, 1, new_n);
         }
     }
     
     if dir != 1{
         let new_n = n - 1;
         if set.contains(&new_n){
-            cnt += dfs(set, 2, new_n) + 1;
+            *cnt += 1;
             set.remove(&new_n);
+            dfs(set, cnt, 2, new_n);
         }
     }
-    
-    cnt
 }
