de3b6d888d1257a686658e176c0cf4934396cde6
Tomi Jaga <jagatomi12@gmail.com>
2022-07-05 21:34:42 -0400

    Time: 3 ms (28.85%), Space: 2 MB (97.12%) - LeetHub

--- 202-happy-number/202-happy-number.rs
+++ 202-happy-number/202-happy-number.rs
@@ -4,17 +4,6 @@ impl Solution {
     pub fn is_happy(mut n: i32) -> bool {
         let mut set = HashSet::new();
         
-        fn split_digits_sum(num: i32)-> i32{
-    let mut num = num;
-    let mut sum = 0_i32;
-    while num > 0{
-        sum += (num % 10).pow(2);
-        num/=10;
-    }
-    
-    sum
-}
-        
         while !set.contains(&n){
             set.insert(n);
             n = split_digits_sum(n);
@@ -29,3 +18,13 @@ impl Solution {
     }
 }
 
+pub fn split_digits_sum(num: i32)-> i32{
+    let mut num = num;
+    let mut sum = 0_i32;
+    while num > 0{
+        sum += (num % 10).pow(2);
+        num/=10;
+    }
+    
+    sum
+}
