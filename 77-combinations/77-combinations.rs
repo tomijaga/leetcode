78d80466e6ae54cdfe44ee968a3015cf46b07322
Tomi Jaga <jagatomi12@gmail.com>
2022-07-06 19:30:55 -0400

    Time: 10 ms (73.85%), Space: 2.9 MB (73.85%) - LeetHub

---
+++ 77-combinations/77-combinations.rs
@@ -0,0 +1,24 @@
+impl Solution {
+    pub fn combine(n: i32, k: i32) -> Vec<Vec<i32>> {
+        fn cbns(start: i32, end: i32, res:&mut Vec<Vec<i32>>, nums: &mut Vec<i32>, k: usize){
+            if nums.len() == k{
+                res.push(nums.to_vec());
+                return;
+            }
+            
+            
+            for n in start..=end{
+                nums.push(n);
+                cbns(n + 1, end, res, nums, k);
+                nums.pop();
+            }
+        }  
+        
+        let mut res = vec![];
+        let mut nums = vec![];
+        
+        cbns(1, n, &mut res, &mut nums, k as usize);
+        
+        res
+    }
+}
