7a17db12f54e4daecff9354c91e24b86dde0b011
Tomi Jaga <jagatomi12@gmail.com>
2022-07-08 20:07:18 -0400

    Time: 2 ms (58.15%), Space: 2.6 MB (14.13%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.rs
+++ 125-valid-palindrome/125-valid-palindrome.rs
@@ -1,15 +1,13 @@
 impl Solution {
     pub fn is_palindrome(s: String) -> bool {
-        let s: Vec<char> = s.to_lowercase()
-            .chars()
+        let s: Vec<char> = s.chars()
             .filter(|c| c.is_alphanumeric())
+            .map(|c| c.to_lowercase().next().unwrap())
             .collect();
-        
-        let (mut left, mut right) = (0, s.len() - 1);
         
         let len = s.len();
         
-        for i in 0..(s.len()/2){
+        for i in 0..(len/2){
             if s[i] != s[len - i - 1]{
                 return false
             }
