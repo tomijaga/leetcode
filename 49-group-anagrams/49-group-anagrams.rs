42347ee9b2b0749df619fe44c0bd6e7eaf52eff9
Tomi Jaga <jagatomi12@gmail.com>
2022-07-09 08:54:05 -0400

    Time: 13 ms (80.37%), Space: 4.9 MB (84.93%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.rs
+++ 49-group-anagrams/49-group-anagrams.rs
@@ -1,27 +1,29 @@
 use std::collections::HashMap;
-use std::str;
-
-pub fn encode(str: &String)-> String{
-    let mut chars = str.chars().collect::<Vec<char>>();
-    chars.sort();
-    
-    chars.into_iter().collect::<String>()
-}
 
 impl Solution {
     pub fn group_anagrams(strs: Vec<String>) -> Vec<Vec<String>> {
-        let mut map:HashMap<String, Vec<String>> = HashMap::new();
+        let mut map: HashMap<[u16; 26], Vec<String>> = HashMap::new();
         
-        for str in strs{
-            let key = encode(&str);
+        for s in strs{
+            let key =  count_chars(&s);
             
             if let Some(vals) = map.get_mut(&key){
-                vals.push(str);
+                vals.push(s);
             }else{
-                map.insert(key, vec![str]);
+                map.insert(key, vec![s]);
             }
         }
         
         map.into_values().collect::<Vec<Vec<String>>>()
     }
+}
+
+pub fn count_chars(s: &str) -> [u16; 26]{
+    let mut arr = [0_u16; 26];
+    
+    for c in s.chars(){
+        arr[(c as u8 - 'a' as u8) as usize] += 1;
+    }
+    
+    arr
 }
