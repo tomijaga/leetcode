1abb80bd2f1df61c04db99985d0482ab5a99bc26
Tomi Jaga <jagatomi12@gmail.com>
2022-07-09 10:33:54 -0400

    Time: 4 ms (71.43%), Space: 2.5 MB (57.14%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.rs
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.rs
@@ -28,36 +28,23 @@ pub fn quick_select(slice: &mut [(i32, i32)], k: i32) -> &[(i32, i32)]{
     
     for index in 1..slice.len(){
         if slice[index].1 >= slice[pivot].1{
-            swap(slice, index, j);
+            slice.swap(index, j);
             j+=1;
         }else{
-            swap(slice, index, i);
+            slice.swap(index, i);
             i+=1;
             j+=1;
         }
     }
     
-    swap(slice, pivot, i - 1);
-    
+    slice.swap(pivot, i - 1);
     pivot = i - 1;
-    i-=1;
     
-    let greater_items = (j - pivot) as i32;
-    // println!("{:?} {:?}", &slice[i..j], greater_items);
+    let larger_items = (j - pivot) as i32;
     
-    match greater_items.cmp(&k) {
-        Ordering::Less => {
-            quick_select(&mut slice[0..j], k)
-        },
-        Ordering::Greater => {
-            quick_select(&mut slice[pivot + 1..j], k)
-        },
+    match larger_items.cmp(&k) {
+        Ordering::Less => quick_select(&mut slice[0..j], k),
+        Ordering::Greater => quick_select(&mut slice[pivot + 1..j], k),
         Ordering::Equal => &slice[pivot..j],
     }
-}
-
-pub fn swap(slice: &mut [(i32, i32)], i: usize, j: usize){
-    let tmp = slice[i];
-    slice[i] = slice[j];
-    slice[j] = tmp;
 }
