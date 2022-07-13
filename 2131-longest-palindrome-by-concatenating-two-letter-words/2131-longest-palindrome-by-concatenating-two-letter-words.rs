9dfb4c1827c215cacc85bd5a8b3029a8766309c0
Tomi Jaga <jagatomi12@gmail.com>
2022-07-13 10:19:21 -0400

    Time: 150 ms (7.41%), Space: 11.4 MB (14.81%) - LeetHub

--- 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.rs
+++ 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.rs
@@ -3,51 +3,42 @@ use std::collections::{ HashMap };
 
 impl Solution {
     pub fn longest_palindrome(words: Vec<String>) -> i32 {
-        let mut map = HashMap::new();
+        let mut map = vec![vec![0; 26]; 26];
         let mut dup_chars_unpaired = 0;        
         let mut cnt = 0;
         
-        for word in words.into_iter().map(|w| w.chars().collect::<Vec<char>>()){
-            
+        let words_iter = words.into_iter()
+            .map(|w| {
+                w.chars()
+                    .map(|c| id(c))
+                    .collect::<Vec<usize>>()
+        });
+        
+        for word in words_iter{
+            let ptr = &mut map[word[0]][word[1]];
             if word[0] == word[1]{
-                
-                if let Some(n) = map.get_mut(&word){
-                    if *n > 0{
-                        *n-=1;
-                        dup_chars_unpaired-=1;
-                        cnt+=2;
-                    }else{
-                        *n += 1;
-                        dup_chars_unpaired+=1;
-                    }
+                if *ptr > 0{
+                    *ptr -=1;
+                    cnt+=4;
+                    dup_chars_unpaired-=1;
                 }else{
-                    map.insert(word, 1);
+                    *ptr +=1;
                     dup_chars_unpaired +=1;
                 }
-                
                 continue;
             }
-            
-            let flipped = vec![word[1], word[0]];
-            
-            if let Some(n) = map.get_mut(&flipped){
-                *n -= 1;
-                
-                if *n >= 0{
-                    cnt +=2;
-                }
-                
-            }else if let Some(n) = map.get_mut(&word){
-                *n += 1;
-                
-                if *n <= 0{
-                    cnt +=2;
-                }
+            if *ptr > 0{
+                *ptr -=1;
+                cnt+=4;
             }else{
-                map.insert(word, 1);
+                map[word[1]][word[0]] += 1;
             }
         }
         
-        (cnt * 2 ) + if dup_chars_unpaired > 0 {2} else {0}
+        cnt + if dup_chars_unpaired > 0 {2} else {0}
     }
+}
+
+pub fn id(c: char) -> usize{
+    c as usize - 'a' as usize
 }
