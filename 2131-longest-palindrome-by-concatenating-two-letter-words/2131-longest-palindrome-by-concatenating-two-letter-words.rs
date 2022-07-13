a1cbf7255f9ec620c02bc4fc739e59c79e588225
Tomi Jaga <jagatomi12@gmail.com>
2022-07-13 10:02:02 -0400

    Time: 215 ms (7.41%), Space: 11.3 MB (14.81%) - LeetHub

--- 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.rs
+++ 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.rs
@@ -1,26 +1,34 @@
-use std::collections::{HashMap, HashSet};
+
+use std::collections::{ HashMap };
 
 impl Solution {
     pub fn longest_palindrome(words: Vec<String>) -> i32 {
         let mut map = HashMap::new();
-        let mut set = HashSet::new();
-        
+        let mut dup_chars_unpaired = 0;        
         let mut cnt = 0;
         
-        for word in words{
-            let word: Vec<char> = word.chars().collect();
-            let flipped = vec![word[1], word[0]];
+        for word in words.into_iter().map(|w| w.chars().collect::<Vec<char>>()){
             
-            if word == flipped{
-                if set.contains(&word){
-                    set.remove(&word);
-                    cnt+=2;
+            if word[0] == word[1]{
+                
+                if let Some(n) = map.get_mut(&word){
+                    if *n > 0{
+                        *n-=1;
+                        dup_chars_unpaired-=1;
+                        cnt+=2;
+                    }else{
+                        *n += 1;
+                        dup_chars_unpaired+=1;
+                    }
                 }else{
-                    set.insert(word.clone());
+                    map.insert(word, 1);
+                    dup_chars_unpaired +=1;
                 }
                 
                 continue;
             }
+            
+            let flipped = vec![word[1], word[0]];
             
             if let Some(n) = map.get_mut(&flipped){
                 *n -= 1;
@@ -36,10 +44,10 @@ impl Solution {
                     cnt +=2;
                 }
             }else{
-                map.insert(word.clone(), 1);
+                map.insert(word, 1);
             }
         }
         
-        (cnt * 2 ) + if set.len() > 0 {2} else {0}
+        (cnt * 2 ) + if dup_chars_unpaired > 0 {2} else {0}
     }
 }
