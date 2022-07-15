cad7148a7c08e1663c371d47d1bb85cd05afb6fd
Tomi Jaga <jagatomi12@gmail.com>
2022-07-14 22:23:51 -0400

    Time: 218 ms (50.62%), Space: 17.7 MB (24.70%) - LeetHub

---
+++ 695-max-area-of-island/695-max-area-of-island.py
@@ -0,0 +1,28 @@
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> bool:
+        def dfs(i: int, j: int)-> int:
+            sum = 0
+            if i >= 0 and j >= 0 and i < m and j < n:
+                if grid[i][j] == 1:
+                    grid[i][j] = 0
+                    sum = 1
+                    for (dx, dy) in [(1 ,0), (0, 1), (-1, 0), (0, -1)]:
+                        x = i + dx
+                        y = j + dy
+                        # if (x, y) == (3, 10) and (i, j) == (4,10):
+                        #     print("FOUND")
+                        sum+=dfs(x, y)
+
+            if sum >=1:
+                print(i, j)
+            return sum
+                    
+        m, n = len(grid), len(grid[0])
+        cnt = 0
+        for i in range(0, m):
+            for j in range(0, n):
+                if grid[i][j] == 1:
+                    cnt = max(dfs(i, j), cnt)
+                    print("---------")
+                    
+        return cnt
