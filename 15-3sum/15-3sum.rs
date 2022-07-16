80e792e3ea361a92acf861e3af4b15dcd05b23ce
Tomi Jaga <jagatomi12@gmail.com>
2022-07-16 00:37:17 -0400

    Time: 40 ms (45.76%), Space: 3.4 MB (66.32%) - LeetHub

--- 15-3sum/15-3sum.rs
+++ 15-3sum/15-3sum.rs
@@ -1,46 +1,40 @@
 impl Solution {
     pub fn three_sum(mut nums: Vec<i32>) -> Vec<Vec<i32>> {
-        nums.sort();
-        
-        // println!("{:?}", &nums);
+        nums.sort_unstable();
         let mut res = vec![];
         
-        for i in 0..(nums.len() as i32 - 2) {
-            let i = i as usize;
+        for i in 0..nums.len() - 1{
+            let n1 = nums[i];
             
-            if i > 0 && nums[i - 1] == nums[i]{
+            if i > 0 && n1 == nums[i - 1]{
                 continue;
             }
             
-            let mut j = i + 1;
-            let mut k = nums.len() - 1;
+            let (mut l, mut r) = (i + 1, nums.len() - 1);
             
-            while j < k{
-                let sum = nums[i] + nums[j] + nums[k];
-                // println!("{:?}, {}", [nums[i], nums[j], nums[k]], sum );
+            while l < r{
+                let n2 = nums[l];
+                let n3 = nums[r];
+                
+                let sum = n1 + n2 + n3;
                 
                 if sum > 0{
-                    k-=1;
+                    r-=1;
                 }else if sum < 0{
-                    j +=1;
+                     l+=1;
                 }else{
-                    let v = vec![nums[i], nums[j], nums[k]];
-                    res.push(v);
-                    
-                    while j < k && nums[j] == nums[j + 1]{
-                        j +=1;
+                    res.push(vec![n1, n2, n3]);
+                    while l < r && n2 == nums[l + 1]{
+                        l+=1;
                     }
+                    l+=1;
                     
-                    while j < k && nums[k] == nums[k - 1]{
-                        k-=1;
+                    while l < r && n3 == nums[r - 1]{
+                        r-=1;
                     }
-                    
-                    j+=1;
-                    k-=1;
+                    r-=1;
                 }
-                
             }
-            
         }
         
         res
