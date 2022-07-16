f0025dfc13631ba8d6776eac9e692dc0210aa5a8
Tomi Jaga <jagatomi12@gmail.com>
2022-07-16 00:14:54 -0400

    Time: 5 ms (33.01%), Space: 2.3 MB (45.38%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.rs
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.rs
@@ -1,16 +1,16 @@
 impl Solution {
     pub fn two_sum(numbers: Vec<i32>, target: i32) -> Vec<i32> {
-        let (mut i , mut j) = (0, numbers.len() - 1);
+        let (mut l, mut r) = (0, numbers.len() - 1);
         
-        while i < j{
-            let sum = numbers[i] + numbers[j];
+        while l < r{
+            let sum = numbers[l] + numbers[r];
             
             if sum > target{
-                j-=1;
+                r-=1;
             }else if sum < target{
-                i+=1;
+                l+=1;
             }else{
-                return vec![(i + 1) as i32, (j + 1) as i32];
+                return vec![(l + 1) as i32, (r + 1) as i32];
             }
         }
         
