f2cc051338faed8070d0862c4624f92e8d2f7e39
Tomi Jaga <jagatomi12@gmail.com>
2022-07-21 11:06:23 -0400

    Time: 1 ms (69.23%), Space: 2.4 MB (15.38%) - LeetHub

--- 78-subsets/78-subsets.rs
+++ 78-subsets/78-subsets.rs
@@ -1,23 +1,25 @@
 impl Solution {
     pub fn subsets(opts: Vec<i32>) -> Vec<Vec<i32>> {
         
-        pub fn sub(opts: &Vec<i32>, i: usize, res: &mut Vec<Vec<i32>>, nums: Vec<i32>){
+        pub fn sub(opts: &Vec<i32>, i: usize, res: &mut Vec<Vec<i32>>, nums: &mut Vec<i32>){
             if i == opts.len(){
-                res.push(nums);
+                res.push(nums.clone());
                 return;
             }
             
-            let mut nums2 = nums.clone();
-            nums2.push(opts[i]);
-            
             let j = i + 1;
             sub(opts, j, res, nums);
-            sub(opts, j, res, nums2);
+            
+            nums.push(opts[i]);
+            sub(opts, j, res, nums);
+            nums.pop();
+            
         }
         
         let mut res = vec![];
+        let mut nums = vec![];
         
-        sub(&opts, 0, &mut res, vec![]);
+        sub(&opts, 0, &mut res, &mut nums);
         
         res
     }
