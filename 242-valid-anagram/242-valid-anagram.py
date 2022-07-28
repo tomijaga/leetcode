5dbc05921aa6fd932adbde98e1bbb4109476a848
Tomi Jaga <jagatomi12@gmail.com>
2022-07-28 14:32:16 -0400

    Time: 90 ms (31.68%), Space: 14.5 MB (66.69%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -1,14 +1,16 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if not (len(s) ==  len(t)):
+        
+        if not len(s)  == len(t):
             return False
         
-        _t = len(t)
+        map = [0] * 26
         
-        for c in s:
-            if c in t:
-                i = t.index(c)
-                t = t[0:i] + t[i+1:_t]
-            else: 
-                return False
-        return True
+        for (a, b) in zip(s, t):
+            if a == b:
+                continue
+                
+            map[ord(a) - ord('a')] +=1;
+            map[ord(b) - ord('a')] -=1;
+            
+        return all([n == 0 for n in map])
