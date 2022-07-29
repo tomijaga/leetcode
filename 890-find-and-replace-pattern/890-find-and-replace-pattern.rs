03d47d0d79c9ef4fc8ce1989fc0e66adcb5cc8ee
Tomi Jaga <jagatomi12@gmail.com>
2022-07-29 13:14:02 -0400

    Time: 1 ms (71.43%), Space: 2.3 MB (14.29%) - LeetHub

--- 890-find-and-replace-pattern/890-find-and-replace-pattern.rs
+++ 890-find-and-replace-pattern/890-find-and-replace-pattern.rs
@@ -2,30 +2,25 @@ use std::collections::HashMap;
 
 impl Solution {
     pub fn find_and_replace_pattern(words: Vec<String>, pattern: String) -> Vec<String> {
-        let mut map = vec!['0'; 26];
-        let mut p_map = vec!['0'; 26];
+        let mut map = HashMap::new();
+        let mut p_map = HashMap::new();
         
         let mut res:Vec<String> = vec![];
 
         for word in words{
             let mut is_match = true;
             for (w, p) in word.chars().zip(pattern.chars()){
-                if map[id(w)] == '0'{
-                    map[id(w)] = p;
-                }
+                map.entry(w).or_insert(p);
+                p_map.entry(p).or_insert(w);
                 
-                if p_map[id(p)] == '0'{
-                    p_map[id(p)] = w;
-                }
-                
-                if map[id(w)] != p || p_map[id(p)] != w{
+                if map.get(&w) != Some(&p) || p_map.get(&p) != Some(&w){
                     is_match = false;
                     continue;
                 }
             }
             
-            map = vec!['0'; 26];
-            p_map = vec!['0'; 26];
+            map.clear();
+            p_map.clear();
             
             if is_match{
                 res.push(word);
@@ -35,8 +30,4 @@ impl Solution {
         res
         
     }
-}
-
-pub fn id(c: char) -> usize{
-    c as usize - 'a' as usize
 }
