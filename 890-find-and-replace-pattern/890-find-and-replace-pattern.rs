b9a0f7337a2da9c49e60d625cca43abc2295e116
Tomi Jaga <jagatomi12@gmail.com>
2022-07-29 12:20:22 -0400

    Time: 0 ms (100.00%), Space: 2.1 MB (14.29%) - LeetHub

--- 890-find-and-replace-pattern/890-find-and-replace-pattern.rs
+++ 890-find-and-replace-pattern/890-find-and-replace-pattern.rs
@@ -4,37 +4,65 @@ impl Solution {
     pub fn find_and_replace_pattern(words: Vec<String>, pattern: String) -> Vec<String> {
         let mut map = HashMap::new();
 
-        let mut mapped_pattern = vec![0; pattern.len()];
-        let mut mapped_word = vec![0; pattern.len()];
+        let mut mapped_pattern:Vec<(i32, i32)> = vec![];
         
         let mut res:Vec<String> = vec![];
         
         let mut cnt = 0;
-        for (i, c) in pattern.chars().enumerate(){
+        for c in pattern.chars(){
             if !map.contains_key(&c){
                 map.insert(c, cnt);
                 cnt+=1;
             }
+            
+            let n = *map.get(&c).unwrap();
                 
-            mapped_pattern[i] = *map.get(&c).unwrap();
+            if let Some(last) = mapped_pattern.last_mut() {
+                if last.0 == n{
+                    last.1 +=1;
+                }else{
+                    mapped_pattern.push((n, 1));
+                }
+            }else{
+                mapped_pattern.push((n, 1));
+            }
         }
+        
+        let mut mapped_word:Vec<(i32, i32)> = Vec::with_capacity(mapped_pattern.len());
         
         for word in words{
             map.clear();
             
             cnt = 0;
-            for (i, c) in word.chars().enumerate(){
+            let mut i = 0;
+            for c in word.chars(){
                 if !map.contains_key(&c){
                     map.insert(c, cnt);
                     cnt+=1;
                 }
                 
-                mapped_word[i] = *map.get(&c).unwrap();
+                let n = *map.get(&c).unwrap();
+                
+                if let Some(last) = mapped_word.last_mut() {
+                    if last.0 == n{
+                        last.1 +=1;
+                    }else{
+                        mapped_word.push((n, 1));
+                    }
+                }else{
+                    mapped_word.push((n, 1));
+                }
+                
+                if mapped_word.len() > mapped_pattern.len(){
+                    break;
+                }
             }
             
             if mapped_word == mapped_pattern{
                 res.push(word);
             }
+            
+            mapped_word.clear();
         }
         
         res
