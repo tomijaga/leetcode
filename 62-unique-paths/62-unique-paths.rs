4612cd9c17ec6d4b30f890e87764f0fbe3fb8e09
Tomi Jaga <jagatomi12@gmail.com>
2022-08-03 22:00:21 -0400

    Time: 2 ms (36.30%), Space: 2 MB (67.41%) - LeetHub

---
+++ 62-unique-paths/62-unique-paths.rs
@@ -0,0 +1,30 @@
+impl Solution {
+    pub fn unique_paths(m: i32, n: i32) -> i32 {
+        let (m, n) = (m as usize, n as usize);
+        
+        let mut grid = vec![vec![0; n]; m];
+        
+        grid[0][0] = 1;
+        
+        for i in 0..m{
+            for j in 0..n{
+                let x = i - 1;
+                let y = j - 1;
+                
+                let mut paths = grid[i][j];
+                
+                if x != usize::MAX && x < m {
+                    paths += grid[x][j];  
+                };
+                
+                if y != usize::MAX && y < n {
+                    paths += grid[i][y];  
+                };
+                
+                grid[i][j] = paths;
+            }
+        }
+        
+        grid[m - 1][n - 1]
+    }
+}
