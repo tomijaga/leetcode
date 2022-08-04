40fce2ea78cb52a7523f775a12234e02c0d5e110
Tomi Jaga <jagatomi12@gmail.com>
2022-08-04 15:49:06 -0400

    Time: 0 ms (100.00%), Space: 2.1 MB (87.50%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.rs
+++ 785-is-graph-bipartite/785-is-graph-bipartite.rs
@@ -2,11 +2,11 @@ use std::collections::{HashMap, HashSet};
 
 impl Solution {
     pub fn is_bipartite(graph: Vec<Vec<i32>>) -> bool {
-        let mut left_set = HashMap::new();
+        let mut colors: Vec<i32> = vec![-1; graph.len()];
         
         (0..graph.len()).all(|i|{
-            if !left_set.contains_key(&(i as i32)){
-                dfs(&graph, &mut left_set, i as i32, true)
+            if colors[i] == -1{
+                dfs(&graph, &mut colors, i, 1)
             }else{
                 true
             }
@@ -16,21 +16,21 @@ impl Solution {
 
 pub fn dfs(
     graph: &Vec<Vec<i32>>, 
-    left_set: &mut HashMap<i32, bool>, 
-    i: i32,
-    is_left: bool
+    colors: &mut Vec<i32>, 
+    i: usize,
+    color: i32
 ) -> bool {
-    // visited.insert(i as i32);
-    left_set.insert(i, is_left);
+    colors[i] = color;
     
     let mut res = true;
-    for &j in graph[i as usize].iter(){
-        if let Some(&pos) = left_set.get(&j){
-            if pos == is_left{
+    for &j in graph[i].iter(){
+        let j = j as usize;
+        if colors[j] != -1{
+            if colors[j] == color{
                 return false;
             }
         }else{
-            res &= dfs(graph, left_set, j, !is_left);
+            res &= dfs(graph, colors, j, !color);
         }
     } 
     
