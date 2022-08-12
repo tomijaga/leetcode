cd5185c98d56e00cb6082551970cec1bea822f3f
Tomi Jaga <jagatomi12@gmail.com>
2022-08-12 08:37:33 -0400

    Time: 7 ms (35.79%), Space: 2.6 MB (12.98%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.rs
+++ 695-max-area-of-island/695-max-area-of-island.rs
@@ -6,7 +6,6 @@ impl Solution {
         for i in 0..m{
             for j in 0..n{
                 if grid[i][j] == 1{
-                    grid[i][j] = 0;
                     area = area.max(dfs(&mut grid, i, j));
                 }
             }
@@ -19,11 +18,12 @@ impl Solution {
 fn dfs(grid: &mut Vec<Vec<i32>>, i: usize, j: usize) -> i32{
     let (m, n) = (grid.len(), grid[0].len());
     
+    grid[i][j] = 0;
+    
     let mut sum = 1;
 
     for (i, j) in [(i+1, j), (i-1, j), (i, j+1), (i, j-1)]{
         if i != usize::MAX && j != usize::MAX && i < m && j < n && grid[i][j] == 1{
-            grid[i][j] = 0;
             sum += dfs(grid, i, j);
         }
     }
