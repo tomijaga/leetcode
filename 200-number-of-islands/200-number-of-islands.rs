03ae0f6b0178c76a89508b72cf8664e066a45423
Tomi Jaga <jagatomi12@gmail.com>
2022-08-12 07:52:54 -0400

    Time: 13 ms (91.50%), Space: 9.1 MB (67.00%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.rs
+++ 200-number-of-islands/200-number-of-islands.rs
@@ -1,29 +1,20 @@
+use std::collections::VecDeque;
+
 impl Solution {
     pub fn num_islands(mut grid: Vec<Vec<char>>) -> i32 {
         let (m, n) = (grid.len(), grid[0].len());
-        
-        let mut stack = vec![];
+        let mut q = VecDeque::new();
         
         let (mut si, mut sj) = (0, 0);
         let mut islands = 0;
         
-        while let Some((i, j))  = get_next_island(&grid, si, sj) {
-            si = i;
-            sj = j;
-            islands+=1;
-            stack.push((i, j));
-
-            while !stack.is_empty(){
-                let (i, j) = stack.pop().unwrap();
-                
+        for i in 0..m{
+            for j in 0..n{
                 if grid[i][j] == '1'{
                     grid[i][j] = '0';
-                    
-                    for (x, y) in [(i + 1, j), (i - 1, j), (i, j+ 1),(i, j-1)]{
-                        if x!=usize::MAX && x < m && y!=usize::MAX && y < n && grid[x][y] == '1'{
-                            stack.push((x, y));
-                        }
-                    }
+                    q.push_back((i, j));
+                    bfs(&mut grid, &mut q);
+                    islands +=1;
                 }
             }
         }
@@ -33,20 +24,17 @@ impl Solution {
     }
 }
 
-fn get_next_island(grid: &Vec<Vec<char>>, startI: usize, startJ: usize) -> Option<(usize, usize)>{
+fn bfs(grid: &mut Vec<Vec<char>>, q: &mut VecDeque<(usize, usize)>){
     let (m, n) = (grid.len(), grid[0].len());
     
-    if startI == 0 && startJ == 0 && grid[startI][startJ] == '1'{
-        return Some((0, 0));
-    }
-    
-    for i in startI..m{
-        for j in 0..n{
-            if (i == startI && j > startJ || i > startI) && grid[i][j] == '1'{
-                return Some((i, j));
+    while !q.is_empty(){
+        let (i, j) = q.pop_front().unwrap();
+        
+        for (x, y) in [(i + 1, j), (i - 1, j), (i, j+ 1),(i, j-1)]{
+            if x!=usize::MAX && x < m && y!=usize::MAX && y < n && grid[x][y] == '1'{
+                q.push_back((x, y));
+                grid[x][y] = '0';
             }
         }
     }
-    
-    None
 }
