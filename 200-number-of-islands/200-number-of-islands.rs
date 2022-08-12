7b256daa898ba95d05cc8de1950208840f367a1f
Tomi Jaga <jagatomi12@gmail.com>
2022-08-12 08:01:17 -0400

    Time: 28 ms (25.50%), Space: 9 MB (93.50%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.rs
+++ 200-number-of-islands/200-number-of-islands.rs
@@ -3,6 +3,7 @@ use std::collections::VecDeque;
 impl Solution {
     pub fn num_islands(mut grid: Vec<Vec<char>>) -> i32 {
         let (m, n) = (grid.len(), grid[0].len());
+        let mut q = VecDeque::new();
         
         let (mut si, mut sj) = (0, 0);
         let mut islands = 0;
@@ -11,7 +12,8 @@ impl Solution {
             for j in 0..n{
                 if grid[i][j] == '1'{
                     grid[i][j] = '0';
-                    bfs(&mut grid, i, j);
+                    q.push_back((i, j));
+                    bfs(&mut grid, &mut q);
                     islands +=1;
                 }
             }
@@ -22,17 +24,16 @@ impl Solution {
     }
 }
 
-fn bfs(grid: &mut Vec<Vec<char>>, i: usize, j: usize){
+fn bfs(grid: &mut Vec<Vec<char>>, q: &mut VecDeque<(usize, usize)>){
     let (m, n) = (grid.len(), grid[0].len());
-    let mut q = VecDeque::from([(i, j)]);
     
     while !q.is_empty(){
         let (i, j) = q.pop_front().unwrap();
         
-        for (x, y) in [(i + 1, j), (i - 1, j), (i, j+ 1),(i, j-1)]{
-            if x!=usize::MAX && x < m && y!=usize::MAX && y < n && grid[x][y] == '1'{
-                q.push_back((x, y));
-                grid[x][y] = '0';
+        for (i, j) in [(i + 1, j), (i - 1, j), (i, j+ 1),(i, j-1)]{
+            if (0..m).contains(&i) && (0..n).contains(&j) && grid[i][j] == '1'{
+                q.push_back((i, j));
+                grid[i][j] = '0';
             }
         }
     }
