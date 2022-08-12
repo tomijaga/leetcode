820f06dc051c0bbe71718931710deadd5318d0db
Tomi Jaga <jagatomi12@gmail.com>
2022-08-11 23:54:55 -0400

    Time: 4 ms (31.43%), Space: 2.1 MB (80.00%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.rs
+++ 994-rotting-oranges/994-rotting-oranges.rs
@@ -2,57 +2,51 @@ use std::collections::VecDeque;
 
 impl Solution {
     pub fn oranges_rotting(mut grid: Vec<Vec<i32>>) -> i32 {
-        let mut rotten = VecDeque::new();
-        let (m,n) = (grid.len(), grid[0].len());
+        let mut q = VecDeque::new();
+        let (m, n) = (grid.len(), grid[0].len());
         
-        // println!("{:?}", (m, n));
+        let (empty, fresh, rotten) = (0, 1, 2);
         let mut fresh_oranges = 0;
+        let mut mins = 0;
         
         for i in 0..m{
             for j in 0..n{
-                if grid[i][j] == 2{
-                    rotten.push_back((i, j));
-                }else if grid[i][j] ==1{
-                    fresh_oranges+=1;
+                if grid[i][j] == rotten{
+                    q.push_back((i, j));
+                }else if grid[i][j] == fresh{
+                    fresh_oranges +=1;
                 }
             }
         }
         
-        let dirs = [(1, 0), (0, 1), (-1, 0), (0, -1)];
-        let mut days = 0;
         
-        while rotten.len() > 0{
-            let mut decayed = false;
-            for _ in 0..rotten.len(){
-                let (i, j) = rotten.pop_front().unwrap();
+        while !q.is_empty(){
+            let mut any_spoiled = false;
+            
+            for _ in 0..q.len(){
+                let (i, j) = q.pop_front().unwrap();
                 
-                for (dx, dy) in dirs.iter(){
-                    let x  = i as i32 + dx;
-                    let y  = j as i32 + dy;
+                for (x, y) in [(i + 1, j), (i - 1, j), (i, j + 1), (i, j-1)]{
                     
-                    if x >= 0 && y>=0 && x < m as i32 && y < n as i32{
-                        let (x, y) = (x as usize, y as usize);
+                    if x!= usize::MAX && y!= usize::MAX && x < m && y < n && grid[x][y] == fresh{
+                        q.push_back((x, y));
+                        fresh_oranges -=1;
                         
-                        if grid[x][y] == 1{
-                            grid[x][y] = 2;
-                            fresh_oranges -=1;
-                            rotten.push_back((x, y));
-                            decayed = true;
-                        }
+                        grid[x][y] = rotten;
+                        any_spoiled = true;
                     }
                 }
             }
             
-            if decayed{
-                days+=1;
+            if any_spoiled{
+                mins +=1;
             }
         }
         
-        if fresh_oranges > 0{
-            -1
+        if fresh_oranges == 0{
+            mins
         }else{
-            days
+            -1
         }
-        
     }
 }
