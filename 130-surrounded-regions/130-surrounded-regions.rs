77f8f587a5e54277c5d73f9a0d022384e6cb6cba
Tomi Jaga <jagatomi12@gmail.com>
2022-08-13 11:42:32 -0400

    Time: 16 ms (34.38%), Space: 4.8 MB (43.75%) - LeetHub

--- 130-surrounded-regions/130-surrounded-regions.rs
+++ 130-surrounded-regions/130-surrounded-regions.rs
@@ -1,49 +1,46 @@
 impl Solution {
     pub fn solve(board: &mut Vec<Vec<char>>) {
-        let (m,n) = (board.len(), board[0].len());
+        let (m, n) = (board.len(), board[0].len());
         
-        for dir in [1, -1]{
-            for i in 0..m{
-                if dir == 1{
-                    dfs(board, i, 0);
-                }else{
-                    dfs(board, m-i -1, n - 1);
-                }
-            }
-            
-            for j in 0..n{
-                if dir == 1{
-                    dfs(board, 0, j);
-                }else{
-                    dfs(board, m-1, n - j -1);
+        // mark_edges
+        for i in 0..m{
+            for j in [0, n-1]{
+                if board[i][j] == 'O'{
+                    dfs(board, i, j);
                 }
             }
         }
         
-        // println!("{:?}", &board);
+        for j in 0..n{
+            for i in [0, m-1]{
+                if board[i][j] == 'O'{
+                    dfs(board, i, j);
+                }
+            }
+        }
         
         for i in 0..m{
             for j in 0..n{
-                if board[i][j] == '#'{
-                    board[i][j] = 'O';
-                }else if board[i][j] == 'O'{
+                if board[i][j] == 'O'{
                     board[i][j] = 'X';
+                }else if board[i][j] == '-'{
+                    board[i][j] = 'O';
                 }
             }
+        }
+        
+    }
+}
+
+fn dfs(board: &mut Vec<Vec<char>>, i: usize, j: usize){
+    let (m, n) = (board.len(), board[0].len());
+    
+    board[i][j] = '-';
+    
+    for (i, j) in [(i+ 1, j), (i-1, j), (i, j+1), (i, j-1)]{
+        if i!=usize::MAX && i < m && j!=usize::MAX && j < n && board[i][j] == 'O'{
+            dfs(board, i, j);
         }
     }
 }
 
-pub fn dfs(board: &mut Vec<Vec<char>>, i: usize, j: usize){
-    let (m,n) = (board.len(), board[0].len());
-    
-    if i!=usize::MAX && i < m && j < n && j!=usize::MAX && board[i][j] == 'O'{
-        board[i][j] = '#';
-        
-        let opts = [(i -1, j), (i + 1, j), (i, j-1), (i, j+1)];
-        
-        for (x, y) in opts{
-            dfs(board, x, y);
-        }
-    }
-}
