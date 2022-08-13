09c0db117be6ef465f371b8968f246889c5d6cc7
Tomi Jaga <jagatomi12@gmail.com>
2022-08-13 12:24:34 -0400

    Time: 13 ms (66.67%), Space: 2.6 MB (50.00%) - LeetHub

--- 286-walls-and-gates/286-walls-and-gates.rs
+++ 286-walls-and-gates/286-walls-and-gates.rs
@@ -21,17 +21,14 @@ impl Solution {
         
         while !q.is_empty(){
             cnt +=1;
-            println!("{:?}", (cnt, &q));
             
             for _ in 0..q.len(){
                 let (i, j) = q.pop_front().unwrap();
 
                 for (i, j) in [(i+1, j), (i-1, j), (i, j+1), (i, j-1)]{
                     if i!=usize::MAX && i < m && j!=usize::MAX && j < n && rooms[i][j] == EMPTY{
-                        if rooms[i][j] > cnt{
-                            rooms[i][j] = cnt;
-                            q.push_back((i, j));
-                        }
+                        rooms[i][j] = cnt;
+                        q.push_back((i, j));
                     }
                 }
             }
