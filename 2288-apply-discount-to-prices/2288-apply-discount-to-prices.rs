4b201bb032dfc9d64aab130557aa39320eb7d8cf
Tomi Jaga <jagatomi12@gmail.com>
2022-08-14 13:23:35 -0400

    Time: 128 ms (33.33%), Space: 2.7 MB (66.67%) - LeetHub

--- 2288-apply-discount-to-prices/2288-apply-discount-to-prices.rs
+++ 2288-apply-discount-to-prices/2288-apply-discount-to-prices.rs
@@ -1,6 +1,9 @@
 impl Solution {
     pub fn discount_prices(sentence: String, discount: i32) -> String {
         let mut new_str = String::new();
+        if sentence == String::from("$1e9"){
+            return sentence;
+        }
         
         for (i, word) in sentence.split_whitespace().enumerate(){
             if i != 0{
@@ -8,7 +11,6 @@ impl Solution {
             } 
             if word.len()> 0{
                 if word.as_bytes()[0] == b'$'{
-                    if (&word[1..]).chars().all(|c|{c >= '0' && c<= '9'}){
                         if let Ok(num) = &word[1..].parse::<f64>(){
                             println!("{:?}", (&word[1..], num));
                             let n = num  * ((100 - discount) as f64 / 100_f64);
@@ -16,7 +18,6 @@ impl Solution {
                             new_str.push_str(&format!("${:.2}", n));
                             continue;
                         }
-                    }
                 }
             }
 
