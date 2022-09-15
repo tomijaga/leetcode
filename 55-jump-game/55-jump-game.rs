44328ad1073503dbcb0c47200f5cb5a46d62cf85
Tomi Jaga <jagatomi12@gmail.com>
2022-09-15 08:47:57 -0400

    Time: 11 ms (53.66%), Space: 2.3 MB (35.37%) - LeetHub

--- 55-jump-game/55-jump-game.rs
+++ 55-jump-game/55-jump-game.rs
@@ -1,27 +1,17 @@
 impl Solution {
     pub fn can_jump(mut nums: Vec<i32>) -> bool {
-        let mut dp = vec![None; nums.len()];
-        let last_ptr = dp.last_mut().unwrap();
-        *last_ptr = Some(true);
+        let mut last_true_index = nums.len() - 1;
         
         for i in (0..nums.len() - 1).rev(){
             let mut res = false;
             
             let upper_bound = (nums.len() - 1 - i).min(nums[i] as usize);
             
-            for di in 1..=upper_bound{
-                let j = i + di;
-                
-                if &Some(true) == &dp[j]{
-                    res = true; 
-                    break;
-                }
+            if i + upper_bound >= last_true_index{
+                last_true_index = i
             }
-            
-            dp[i] = Some(res);
-            
         }
         
-        &Some(true) == &dp[0]
+        last_true_index == 0
     }
 }
