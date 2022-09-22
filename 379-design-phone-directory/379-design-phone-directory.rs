2096e1c1a7e7bec36d4009253931da16dbe26a40
Tomi Jaga <jagatomi12@gmail.com>
2022-09-22 11:31:57 -0400

    Time: 21 ms (100.00%), Space: 5.5 MB (100.00%) - LeetHub

--- 379-design-phone-directory/379-design-phone-directory.rs
+++ 379-design-phone-directory/379-design-phone-directory.rs
@@ -1,7 +1,6 @@
 use std::collections::{BTreeSet, BinaryHeap};
 
 struct PhoneDirectory {
-    bitvec: Vec<i32>,
     nbits: i32,
     n: i32,
     released: BTreeSet<i32>
@@ -11,40 +10,22 @@ impl PhoneDirectory {
 
     fn new(maxNumbers: i32) -> Self {
         Self{
-            bitvec: vec![0; (maxNumbers/32) as usize + 1],
             nbits: maxNumbers,
             n: 0,
             released: BTreeSet::new()
         }
     }
-    
-    fn pos(&self, n: i32) -> (usize, usize){
-        ((n / 32) as usize, (n % 32) as usize)
-    }
-    
-    fn set(&mut self, n: i32, b: bool){
-        let (r, c) = self.pos(n);
-        
-        if b {
-            self.bitvec[r] |= 1 << c;
-        }else{
-            self.bitvec[r] &= !(1 << c);
-        }
-    }
-    
+
     fn get(&mut self) -> i32 {
         
         if self.n < self.nbits{
             let val = self.n;
             self.n+=1;
 
-            self.set(val, true);
-
             return val;
         }
         
         if let Some(&n) = self.released.iter().next(){
-            self.set(n, true);
             self.released.remove(&n);
             return n;
         }
@@ -53,14 +34,12 @@ impl PhoneDirectory {
     }
     
     fn check(&self, number: i32) -> bool {
-        let (r, c) = self.pos(number);
-        // println!("{:b}", &self.bitvec[0]);
-        (self.bitvec[r] & 1<<c) == 0
+        // println!("{:?}", (number >= self.n, !self.released.contains(&number)));
+        number >= self.n || self.released.contains(&number)
     }
     
     fn release(&mut self, number: i32) {
         if number < self.n{
-            self.set(number, false);
             self.released.insert(number);
         }
     }
