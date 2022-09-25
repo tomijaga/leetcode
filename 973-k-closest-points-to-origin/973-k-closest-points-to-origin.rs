777f4600eb798de259f8246ec5c7489960f99792
Tomi Jaga <jagatomi12@gmail.com>
2022-09-25 14:21:26 -0400

    Time: 47 ms (73.79%), Space: 3.3 MB (30.10%) - LeetHub

---
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.rs
@@ -0,0 +1,22 @@
+use std::collections::BinaryHeap;
+
+impl Solution {
+    pub fn k_closest(points: Vec<Vec<i32>>, k: i32) -> Vec<Vec<i32>> {
+        let k = k as usize;
+        let mut heap = BinaryHeap::new();
+        
+        for point in points{
+            let (x, y) = (point[0], point[1]);
+            let dx = (x - 0).abs().pow(2);
+            let dy = (y - 0).abs().pow(2);
+            
+            heap.push((dx + dy, vec![x, y]));
+            
+            if heap.len() > k{
+                heap.pop();
+            }
+        }
+        
+        heap.into_iter().map(|(d, v)| v).collect()
+    }
+}
