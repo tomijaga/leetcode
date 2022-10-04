839efa105bfcd88fe21bdb90eb2aa62b8eaea835
Tomi Jaga <jagatomi12@gmail.com>
2022-10-04 16:50:14 -0400

    Time: 0 ms (100.00%), Space: 2.1 MB (26.47%) - LeetHub

--- 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.rs
+++ 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.rs
@@ -1,21 +1,20 @@
 impl Solution {
     pub fn are_almost_equal(s1: String, s2: String) -> bool {
-        let mut map_a = vec![0; 26];
-        let mut map_b = vec![0; 26];
+        let mut map = vec![0; 26];
         
         let len = s1.len();
         let mut matches = 0;
         
         for (a, b) in s1.chars().zip(s2.chars()){
-            map_a[id(a)] +=1;
-            map_b[id(b)] +=1;
+            map[id(a)] +=1;
+            map[id(b)] -=1;
             
             matches += if a == b { 1 } else { 0 };
         }
         
         let diff = len - matches;
 
-        map_a == map_b && (diff == 0 || diff == 2)
+        map.into_iter().all(|cnt| cnt == 0) && (diff == 0 || diff == 2)
     }
 }
 
