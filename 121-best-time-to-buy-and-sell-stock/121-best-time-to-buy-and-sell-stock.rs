04176b86f515fffd8af7575699f1a1739420cb21
Tomi Jaga <jagatomi12@gmail.com>
2022-10-09 15:06:21 -0400

    Time: 12 ms (94.42%), Space: 2.9 MB (66.34%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.rs
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.rs
@@ -1,20 +1,14 @@
 impl Solution {
     pub fn max_profit(prices: Vec<i32>) -> i32 {
+        
+        let mut min_price = i32::MAX;
         let mut max_profit = 0;
         
-        let mut prev_max = i32::MAX;
-        let mut buy_price = i32::MAX;
-        
         for price in prices{
+            min_price = min_price.min(price);
             
-            prev_max = prev_max.max(price);
-            let profit = prev_max - buy_price;
+            let profit = price - min_price;
             max_profit = max_profit.max(profit);
-            
-            if price < buy_price{
-                buy_price = price;
-                prev_max = price;
-            }
         }
         
         max_profit
