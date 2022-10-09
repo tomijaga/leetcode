173d61cc4b52d971505ca47806e903da81732f81
Tomi Jaga <jagatomi12@gmail.com>
2022-10-09 15:43:39 -0400

    Time: 0 ms (100.00%), Space: 2.3 MB (39.24%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.rs
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.rs
@@ -1,24 +1,24 @@
-use std::{
-    collections::HashMap,
-    cmp
-};
+use std::collections::HashMap;
 
 impl Solution {
     pub fn length_of_longest_substring(s: String) -> i32 {
         let mut map = HashMap::new();
-        
-        let mut max = 0;
+        let mut max_len = 0;
         let mut i = 0;
         
         for (j, c) in s.chars().enumerate(){
-            if let Some(dup_index) = map.get(&c){
-                i = cmp::max(dup_index + 1, i);
+            if let Some(&dup_index) = map.get(&c){
+                i = i.max(dup_index + 1);
             }
-                
+            
             map.insert(c, j);
-            max = cmp::max(max, j - i + 1);
+            max_len = max_len.max(j - i + 1);
         }
-        
-        max as i32
+
+        max_len as i32
     }
+}
+
+fn id(c: char) -> usize{
+    (c as u8 - 'a' as u8) as usize
 }
