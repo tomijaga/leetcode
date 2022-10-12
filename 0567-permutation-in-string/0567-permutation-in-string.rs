250181add6f67f71ba048fecbde69dd8aa829d6f
Tomi Jaga <jagatomi12@gmail.com>
2022-10-11 20:03:57 -0400

    Time: 0 ms (100.00%), Space: 2.1 MB (59.38%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.rs
+++ 0567-permutation-in-string/0567-permutation-in-string.rs
@@ -1,12 +1,12 @@
 impl Solution {
     pub fn check_inclusion(s1: String, s2: String) -> bool {
-        
         if s1.len() > s2.len(){
             return false;
         }
         
         let mut map1 = vec![0; 26];
         let mut map2 = vec![0; 26];
+        
         let len = s1.len();
         
         for (a, b) in s1.chars().zip(s2.chars()).take(len){
@@ -14,17 +14,40 @@ impl Solution {
             map2[id(b)] += 1;
         }
         
-        if map1 == map2{
+        let mut matches = 0;
+        
+        for (&a, &b) in map1.iter().zip(map2.iter()){
+            if a == b {
+                matches +=1;
+            }
+        }
+        
+        if matches == 26{
             return true;
         }
         
-        let mut s2_iter = s2.chars();
-        
-        for (prev, curr) in s2_iter.clone().zip(s2_iter.skip(len)){
+        for (prev, curr) in s2.chars().zip(s2.chars().skip(len)){
+            if map2[id(prev)] == map1[id(prev)]{
+                matches -=1;
+            }
+            
             map2[id(prev)] -= 1;
+            
+            if map2[id(prev)] == map1[id(prev)]{
+                matches +=1;
+            }
+            
+            if map2[id(curr)] == map1[id(curr)]{
+                matches -=1;
+            }
+            
             map2[id(curr)] += 1;
             
-            if map1 == map2{
+            if map2[id(curr)] == map1[id(curr)]{
+                matches +=1;
+            }
+
+            if matches == 26{
                 return true;
             }
         }
