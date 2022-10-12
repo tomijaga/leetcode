11d12b31b19739f1385e14ae38c1c654a2f0bbac
Tomi Jaga <jagatomi12@gmail.com>
2022-10-11 20:44:51 -0400

    Time: 3 ms (72.73%), Space: 2.8 MB (27.27%) - LeetHub

--- 0252-meeting-rooms/0252-meeting-rooms.rs
+++ 0252-meeting-rooms/0252-meeting-rooms.rs
@@ -1,13 +1,15 @@
 impl Solution {
     pub fn can_attend_meetings(mut intervals: Vec<Vec<i32>>) -> bool {
         intervals.sort_unstable();
+        let mut iter = intervals.into_iter();
         
-        for w in intervals.windows(2){
-            let a = &w[0];
-            let b = &w[1];
-            
-            if a[1] > b[0]{
-                return false;
+        if let Some(mut prev) = iter.next(){
+            for curr in iter{
+                if prev[1] > curr[0]{
+                    return false;
+                }
+
+                prev = curr;
             }
         }
         
