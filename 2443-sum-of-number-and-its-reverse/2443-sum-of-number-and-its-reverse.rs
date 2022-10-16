6ef6b737822c11dafaee7bda6a54148edaf65955
Tomi Jaga <jagatomi12@gmail.com>
2022-10-16 01:25:02 -0400

    Time: 40 ms (100.00%), Space: 2 MB (100.00%) - LeetHub

---
+++ 2443-sum-of-number-and-its-reverse/2443-sum-of-number-and-its-reverse.rs
@@ -0,0 +1,25 @@
+impl Solution {
+    pub fn sum_of_number_and_reverse(num: i32) -> bool {
+        if num == 0 { return true };
+        let len = (num as f32).log10().ceil() as i32;
+        
+        for n in (num/2..num).rev(){
+            if n + reverse(n) == num{
+                return true;
+            }
+        }
+        
+        false
+    }
+}
+
+fn reverse(mut n: i32) -> i32 {
+    let mut reversed = 0;
+    
+    while n > 0{
+        reversed = (n % 10) + (reversed * 10);
+        n/= 10;
+    }
+    
+    reversed
+}
