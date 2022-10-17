8f7a928690f05e1e7c41590dd1608c2c88bdcabc
Tomi Jaga <jagatomi12@gmail.com>
2022-10-16 23:06:58 -0400

    Time: 65 ms (13.64%), Space: 2.9 MB (27.27%) - LeetHub

--- 1656-design-an-ordered-stream/1656-design-an-ordered-stream.rs
+++ 1656-design-an-ordered-stream/1656-design-an-ordered-stream.rs
@@ -1,25 +1,26 @@
+use std::collections::HashMap;
+
+#[derive(Default)]
 struct OrderedStream {
     ptr : usize,
-    store : Vec<Option<String>>
+    store : HashMap<usize, String>
 }
 
 
 impl OrderedStream {
 
     fn new(n: i32) -> Self {
-        Self{
-            ptr: 0,
-            store : vec![None; n as usize]
-        }
+        Default::default()
     }
     
     fn insert(&mut self, id_key: i32, value: String) -> Vec<String> {
         let id_key = (id_key - 1) as usize;
-        self.store[id_key] = Some(value);
+        self.store.insert(id_key, value);
+        
         let mut res = vec![];
         
         if id_key == self.ptr{
-            while let Some(Some(val)) = self.store.get(self.ptr){
+            while let Some(val) = self.store.remove(&self.ptr){
                 res.push(val.clone());
                 self.ptr+=1;
             }
