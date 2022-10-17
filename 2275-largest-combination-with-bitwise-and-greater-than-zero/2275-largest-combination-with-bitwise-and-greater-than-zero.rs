724c76fd88786f7d5d0e5ea12e284bf74f965261
Tomi Jaga <jagatomi12@gmail.com>
2022-10-17 19:43:37 -0400

    Time: 41 ms (50.00%), Space: 15.3 MB (50.00%) - LeetHub

--- 2275-largest-combination-with-bitwise-and-greater-than-zero/2275-largest-combination-with-bitwise-and-greater-than-zero.rs
+++ 2275-largest-combination-with-bitwise-and-greater-than-zero/2275-largest-combination-with-bitwise-and-greater-than-zero.rs
@@ -1,18 +1,19 @@
 impl Solution {
-    pub fn largest_combination(candidates: Vec<i32>) -> i32 {
+    pub fn largest_combination(mut candidates: Vec<i32>) -> i32 {
         let mut arr = vec![vec![0_i32;candidates.len()];32];
+        let mut sum = 0;
+        let mut max_len = 0;
         
-        for (i, n) in candidates.into_iter().enumerate(){
-            for (j, pos) in (0..32).enumerate(){
-                arr[j][i] = 1 & (n >> pos);
-            }
+        for _ in 0..32{
+            for n in candidates.iter_mut(){
+                sum += 1 & *n;
+                *n = *n >> 1;
+            }    
+            
+            max_len = max_len.max(sum);
+            sum = 0;
         }
         
-        arr.into_iter()
-            .map(|v| {
-                v.into_iter().sum::<i32>()
-            })
-            .max()
-            .unwrap()
+        max_len
     }
 }
