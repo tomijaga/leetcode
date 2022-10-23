aed675827412c8da39808111981eb85243e752dc
Tomi Jaga <jagatomi12@gmail.com>
2022-10-22 22:27:20 -0400

    Time: 14 ms (17.14%), Space: 2.3 MB (48.57%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.rs
@@ -0,0 +1,21 @@
+use std::collections::HashSet;
+
+impl Solution {
+    pub fn find_error_nums(nums: Vec<i32>) -> Vec<i32> {
+        let mut set = HashSet::new();
+        let mut unique_sum = 0;
+        let mut total_sum = 0;
+        
+        let mut expected_sum = ((nums.len() * (nums.len() + 1))/ 2) as i64;
+        
+        for n in nums{
+            if !set.contains(&n){
+                unique_sum += n as i64;
+            }
+            total_sum +=  n as i64;
+            set.insert(n);
+        }
+        
+        vec![(total_sum - unique_sum) as i32, (expected_sum - unique_sum) as i32]
+    }
+}
