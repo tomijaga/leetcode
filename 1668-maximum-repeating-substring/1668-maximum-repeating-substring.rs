9d86753421d96ad349eb42fe97349d12cc2415ed
Tomi Jaga <jagatomi12@gmail.com>
2022-10-24 21:24:20 -0400

    Time: 3 ms (25.00%), Space: 2.1 MB (25.00%) - LeetHub

--- 1668-maximum-repeating-substring/1668-maximum-repeating-substring.rs
+++ 1668-maximum-repeating-substring/1668-maximum-repeating-substring.rs
@@ -1,44 +1,15 @@
 use std::collections::HashMap;
 
 impl Solution {
-    pub fn max_repeating(sequence: String, word: String) -> i32 {
-        if word.len() > sequence.len() {
-            return 0;
-        }
-        
-        let mut map = HashMap::new();
-        
-        let word_len = word.len();
-        let limit = sequence.len() - word.len();
-        
+    pub fn max_repeating(mut sequence: String, mut word: String) -> i32 {
         let mut cnt = 0;
-        let mut max_len = 0;
-        let mut i = 0;
+        let mut words = vec![word.clone()];
         
-        for i in 0..=limit{
-            if sequence[i..(i + word_len)] == word[..]{
-                map.entry(i % word_len).or_insert(vec![]).push(i / word_len);
-            }
+        while let Some(_) =  sequence.find(&(words.concat())){
+            words.push(word.clone());
+            cnt+=1;
         }
         
-        for v in map.into_values(){
-            // println!("{:?}", &v);
-            cnt = 1;
-            for w in v.windows(2){
-                let a = w[0];
-                let b = w[1];
-
-                if a + 1 == b{
-                    cnt += 1;
-                }else{
-                    cnt = 1;
-                }
-
-                max_len = max_len.max(cnt);
-            }
-            max_len = max_len.max(cnt);
-        }
-
-        max_len
+        cnt
     }
 }
