4046a5ba9736327ba01a10f80531c8b558481dff
Tomi Jaga <jagatomi12@gmail.com>
2022-10-28 23:12:58 -0400

    Time: 7 ms (75.00%), Space: 2.1 MB (75.00%) - LeetHub

--- 0487-max-consecutive-ones-ii/0487-max-consecutive-ones-ii.rs
+++ 0487-max-consecutive-ones-ii/0487-max-consecutive-ones-ii.rs
@@ -1,9 +1,6 @@
-use std::collections::VecDeque;
-
 impl Solution {
     pub fn find_max_consecutive_ones(nums: Vec<i32>) -> i32 {
-        let mut q : VecDeque<(i32, i32)> = VecDeque::new();
-        let mut flipped = 0;
+        let mut flipped = false;
         let mut max_ones = 0;
         let mut cnt = 0;
                         //prev, after flipped zero
@@ -11,28 +8,23 @@ impl Solution {
         
         for n in nums{
             if n == 0{
-                if flipped == 1{
-                    state.1 = cnt - 1 - state.0;
+                if flipped{
+                    state.1 = cnt - state.0 - 1;
                     let prev_state = state;
-                    
-                    if (prev_state != (0, 0)){
-                        q.push_back(prev_state);
-                    }
                     
                     state = (prev_state.1, 0);
                     cnt -= prev_state.0;
                 }else{
-                    flipped = 1;
+                    flipped = true;
                     state.0 = cnt;
                     cnt += 1;
                 }
             }else{
                 cnt+=1;
             }
+            
             max_ones = max_ones.max(cnt);
         }
-        
-        // println!("{:?}", q);
         
         max_ones
     }
