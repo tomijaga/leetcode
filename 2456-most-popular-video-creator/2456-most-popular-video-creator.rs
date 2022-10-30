7d89124f87ea7d78af5841d6f41120fbe22db46e
Tomi Jaga <jagatomi12@gmail.com>
2022-10-30 16:14:50 -0400

    Time: 162 ms (100.00%), Space: 27.9 MB (100.00%) - LeetHub

--- 2456-most-popular-video-creator/2456-most-popular-video-creator.rs
+++ 2456-most-popular-video-creator/2456-most-popular-video-creator.rs
@@ -3,8 +3,7 @@ use std::collections::{HashMap, BinaryHeap};
 impl Solution {
     pub fn most_popular_creator(creators: Vec<String>, ids: Vec<String>, views: Vec<i32>) -> Vec<Vec<String>> {
         
-        let mut view_map = HashMap::new();
-        let mut id_map = HashMap::new();
+        let mut map = HashMap::new();
         
         let mut max_sum = 0;
         
@@ -15,26 +14,25 @@ impl Solution {
         for ((creator, id), views) in a.zip(b).zip(c){
             
             let new_movie = (views, id);
-            let movie = id_map.entry(creator.clone()).or_insert(new_movie.clone());
+            let entry = map.entry(creator.clone()).or_insert((0, new_movie.clone()));
+            let (mut total_views, movie) = entry;
             
             if new_movie.0 > movie.0{
-                *movie = new_movie;
+                entry.1 = new_movie;
             }else if movie.0 == new_movie.0 && new_movie.1 < movie.1{
-                *movie = new_movie;
+                entry.1 = new_movie;
             }
             
-            let sum = view_map.entry(creator).or_insert(0_i64);
-            *sum = *sum + views as i64;
-            
-            max_sum = max_sum.max(*sum);
+            total_views = total_views + views as i64;
+            entry.0 = total_views;
+            max_sum = max_sum.max(total_views);
         }
         
         let mut res = vec![];
         
-        for (creator, views) in view_map{
-            if views == max_sum{
-                let id = id_map.remove(&creator).unwrap().1;
-                res.push(vec![creator, id]);
+        for (creator, (total_views, movie)) in map{
+            if total_views == max_sum{
+                res.push(vec![creator, movie.1]);
             }
         }
         
