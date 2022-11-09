f3387cf62cb5b5fef5b14b81c765ba6ed4b8e2de
Tomi Jaga <jagatomi12@gmail.com>
2022-11-09 15:38:48 -0500

    Time: 10 ms (69.23%), Space: 4.4 MB (5.13%) - LeetHub

--- 1198-find-smallest-common-element-in-all-rows/1198-find-smallest-common-element-in-all-rows.rs
+++ 1198-find-smallest-common-element-in-all-rows/1198-find-smallest-common-element-in-all-rows.rs
@@ -1,25 +1,31 @@
-use std::collections::{BTreeSet, HashSet};
+use std::collections::{HashSet};
 use std::iter::FromIterator;
 
 impl Solution {
     pub fn smallest_common_element(mat: Vec<Vec<i32>>) -> i32 {
-        let mut ordered_set = BTreeSet::from_iter(mat[0].clone());
+        let mut main_set: HashSet<i32> = HashSet::from_iter(mat[0].clone());
         let mut removes = vec![];
         
         for arr in mat.into_iter().skip(1){
             let set: HashSet<i32> = HashSet::from_iter(arr);
             
-            for n in ordered_set.iter().cloned(){
+            for n in main_set.iter().cloned(){
                 if !set.contains(&n){
                     removes.push(n);
                 }
             }
             
             while let Some(n) = removes.pop(){
-                ordered_set.remove(&n);
+                main_set.remove(&n);
             }
         }
         
-        ordered_set.into_iter().next().unwrap_or(-1)
+        let mut min_val = i32::MAX;
+        
+        for n in main_set{
+            min_val = min_val.min(n);
+        }
+        
+        if min_val == i32::MAX { -1 }else { min_val }
     }
 }
