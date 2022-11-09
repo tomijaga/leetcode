d245c03ef3ea6c44968ebb592a33ead605fd2d05
Tomi Jaga <jagatomi12@gmail.com>
2022-11-09 14:25:16 -0500

    Time: 68 ms (75.00%), Space: 6.5 MB (25.00%) - LeetHub

---
+++ 0901-online-stock-span/0901-online-stock-span.rs
@@ -0,0 +1,38 @@
+#[derive(Default)]
+struct StockSpanner {
+            // (val, pos)
+    stack: Vec<(i32, i32)>,
+    index : i32
+}
+
+impl StockSpanner {
+
+    fn new() -> Self {
+        Default::default()
+    }
+    
+    fn next(&mut self, price: i32) -> i32 {
+        self.index += 1;
+        
+        let mut cnt = 0;
+        let mut span = self.index - 0;
+        
+        while let Some(&(prev_val, prev_index)) = self.stack.last(){
+            if prev_val <= price{
+                self.stack.pop();
+            }else{
+                span = self.index - prev_index;
+                break;
+            }
+        }
+        
+        if self.stack.is_empty(){
+            span = self.index;
+        };
+        
+        
+        self.stack.push((price, self.index));
+        // println!("{:?}", &self.stack);
+        span
+    }
+}
