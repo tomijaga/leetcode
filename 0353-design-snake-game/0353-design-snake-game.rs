7240f0741174471b28156176f58bf4c1e4e0832b
Tomi Jaga <jagatomi12@gmail.com>
2022-11-11 20:27:15 -0500

    Time: 63 ms (33.33%), Space: 5 MB (66.67%) - LeetHub

--- 0353-design-snake-game/0353-design-snake-game.rs
+++ 0353-design-snake-game/0353-design-snake-game.rs
@@ -3,8 +3,8 @@ use std::iter::FromIterator;
 
 #[derive(Default)]
 struct SnakeGame {
-    set: HashSet<Vec<i32>>,
-    snake : VecDeque<Vec<i32>>,
+    set: HashSet<(i32, i32)>,
+    snake : VecDeque<(i32, i32)>,
     food : Vec<Vec<i32>>,
     height: i32,
     width: i32,
@@ -19,33 +19,41 @@ impl SnakeGame {
         Self{
             height,
             width,
-            snake: VecDeque::from([vec![0;2]]),
-            set: HashSet::from_iter([vec![0;2]]),
+            snake: VecDeque::from([(0, 0)]),
+            set: HashSet::from_iter([(0, 0)]),
             food
         }
     }
     
     fn make_a_move(&mut self, direction: String) -> i32 {
-        let mut head = self.snake.front().unwrap().clone();
+        let mut head = *self.snake.front().unwrap();
         
         match &direction[..]{
-            "R" => head[1] += 1,
-            "L" => head[1] -= 1,
-            "U" => head[0] -= 1,
-            "D" => head[0] += 1,
+            "R" => head.1 += 1,
+            "L" => head.1 -= 1,
+            "U" => head.0 -= 1,
+            "D" => head.0 += 1,
             _ => unreachable!()
         }
         
-        self.snake.push_front(head.clone());
+        self.snake.push_front(head);
 
-        if self.snake.front() != self.food.last(){
+        let mut ate_food = false;
+        
+        if let Some(food) = self.food.last(){
+            if head.0 == food[0] && head.1 == food[1]{
+                ate_food = true;
+            }
+        }
+        
+        if ate_food{
+            self.food.pop();
+        }else{
             let tail =  self.snake.pop_back().unwrap();
             self.set.remove(&tail);
-        }else{
-            self.food.pop();
         }
 
-        if head[0] >= 0 && head[0] < self.height && head[1] >= 0 && head[1] < self.width && !self.set.contains(&head) {
+        if head.0 >= 0 && head.0 < self.height && head.1 >= 0 && head.1 < self.width && !self.set.contains(&head) {
             self.set.insert(head);
             (self.snake.len() - 1) as i32
         }else{
