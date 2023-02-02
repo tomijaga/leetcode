b6c00576faf32936ffd226889c0337e18a5494cc
Tomi Jaga <jagatomi12@gmail.com>
2023-02-01 19:36:41 -0500

    Time: 7 ms (40.00%), Space: 2.4 MB (80.00%) - LeetHub

--- 0622-design-circular-queue/0622-design-circular-queue.rs
+++ 0622-design-circular-queue/0622-design-circular-queue.rs
@@ -1,60 +1,67 @@
-use std::collections::VecDeque;
-
+#[derive(Default)]
 struct MyCircularQueue {
-    store: VecDeque<i32>,
-    capacity : usize,
+    start: usize,
+    count: usize,
+    storage: Vec<i32>
 }
-
 
 impl MyCircularQueue {
 
     fn new(k: i32) -> Self {
-        let k = k as usize;
-        Self{
-            store: VecDeque::new(),
-            capacity: k,
+        Self { 
+            storage: vec![0; k as usize],
+            ..Default::default()
         }
+    }
+    
+    fn get_index(&self, i: usize) -> usize{
+        (self.start + i) % self.storage.capacity()
     }
     
     fn en_queue(&mut self, value: i32) -> bool {
-        if self.store.len() < self.capacity{
-            self.store.push_back(value);
-            return true;
+        if (self.count == self.storage.capacity()){
+            return false;
         }
         
-        false
+        let index = self.get_index(self.count);
+        self.storage[index] = value;
+        self.count += 1;
+        true
     }
     
     fn de_queue(&mut self) -> bool {
-        if self.store.len() > 0{
-            self.store.pop_front();
-            return true;
+        if (self.count == 0){
+            return false;
         }
         
-        false
+        self.count -= 1;
+        self.start = self.get_index(1);
+        
+        return true;
     }
     
     fn front(&self) -> i32 {
-        if self.store.len() > 0{
-            *self.store.front().unwrap()
-        }else{
+        if (self.count == 0){
             -1
+        }else{
+            self.storage[self.start]
         }
     }
     
     fn rear(&self) -> i32 {
-        if self.store.len() > 0{
-            *self.store.back().unwrap()
-        }else{
-            -1
+        if (self.count == 0){
+            return -1;
         }
+        
+        let index = self.get_index(self.count - 1);
+        self.storage[index]
     }
     
     fn is_empty(&self) -> bool {
-        self.store.is_empty()
+        self.count == 0
     }
     
     fn is_full(&self) -> bool {
-        self.store.len() == self.capacity
+        self.count == self.storage.capacity()   
     }
 }
